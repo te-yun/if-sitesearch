3957208a7bc7eb959a61817a3cc146eb8d64a25f
Alex <alexander.orlov@intrafind.de>
2017-07-19 10:57:04 +0200

    update Docker spec

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -9,9 +9,8 @@ DOCKER_TAG=latest
 
 # docker login -u intrafind
 docker build --tag=intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG .
-#docker push intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
+docker push intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
 docker rm -f $DOCKER_IMAGE_NAME
-#docker run -d -p 80:8001 --name $DOCKER_IMAGE_NAME intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
-docker run -t -p 8001:8001 --name $DOCKER_IMAGE_NAME intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
+docker run -d -p 80:8001 --name $DOCKER_IMAGE_NAME intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
 
 docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
