0d77c91311e4bf3b014b6d013fc732e39240b1ff
Alex <alexander.orlov@intrafind.de>
2017-07-20 16:41:27 +0200

    update docker-compose

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # TODO make the container accessible on host only, not to the Internet!
-docker rm -f sitesearch
-#docker run -t --name sitesearch \
-docker run --name sitesearch \
+docker rm -f elasticsearch
+#docker run -t --name elasticsearch \
+docker run --name elasticsearch \
     -p 9200-9303:9200-9303 \
     elasticsearch:2
 #    elasticsearch:2-alpine
