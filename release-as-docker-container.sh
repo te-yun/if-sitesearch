d697126000f716047214971dc1848513ca36a679
Alex <alexander.orlov@loxal.net>
2017-07-25 12:21:27 +0200

    fix Docker deployment

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -6,15 +6,11 @@ echo `ls build/libs/*.jar | sed "s/.*\/\(.\+\)-.*\.jar/\1/"`
 DOCKER_IMAGE_NAME=`ls build/libs/*.jar | sed "s/.*\/\(.\+\)-.*\.jar/\1/"`
 DOCKER_TAG=latest
 
-# docker login -u intrafind
 docker build --tag intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG .
-#docker push intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
 docker rm -f $DOCKER_IMAGE_NAME
 docker run -d --name $DOCKER_IMAGE_NAME \
     -p 80:8001 \
-    -v ~/srv/$DOCKER_IMAGE_NAME:/data \
+    -v ~/srv/$DOCKER_IMAGE_NAME:data \
     intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
-#    -v ~/srv/$DOCKER_IMAGE_NAME:/home/app_user/data \
-#    -v /home/alexander_orlov/tmp/srv/$DOCKER_IMAGE_NAME:/home/app_user/data \
 
 docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
