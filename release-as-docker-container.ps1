c8f767a74172a8f158cafa7642d1bf29b55bfd1d
Alex <alexander.orlov@loxal.net>
2017-07-25 12:27:23 +0200

    fix Docker deployment

--- release-as-docker-container.ps1
+++ release-as-docker-container.ps1
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 ./gradlew clean build
 
 echo `ls build/libs/*.jar | sed "s/.*\/\(.\+\)-.*\.jar/\1/"`
@@ -8,9 +6,6 @@ DOCKER_TAG=latest
 
 docker build --tag intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG .
 docker rm -f $DOCKER_IMAGE_NAME
-docker run -d --name $DOCKER_IMAGE_NAME \
-    -p 80:8001 \
-    -v ~/srv/$DOCKER_IMAGE_NAME:/data \
-    intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
+docker run -d --name $DOCKER_IMAGE_NAME -p 80:8001 -v ~/srv/$DOCKER_IMAGE_NAME:/data intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
 
 docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
