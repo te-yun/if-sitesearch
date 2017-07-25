2cff44571c6d4008b4c7b929c21395b68bc9f234
Alex <alexander.orlov@loxal.net>
2017-07-25 12:04:34 +0200

    map data folder

--- run.ps1
+++ run.ps1
@@ -1,11 +1,4 @@
 Function Main {
-#    SPRING_CONFIG_NAME=local,application \
-        ./gradlew clean bootRun \
-            --continuous \
-            --parallel \
-            --build-cache \
-            --no-rebuild \
-            --no-scan \
-            --debug-jvm
+    ./gradlew clean bootRun --continuous --parallel --build-cache --no-rebuild --no-scan --debug-jvm
 }
-main
+Main
