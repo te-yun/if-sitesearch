ce7f49c39d544e2e855593bd4b621f69eeab5603
Alex <alexander.orlov@loxal.net>
2017-07-28 15:04:14 +0200

    do not expose any ports through elasticsearch container

--- bootstrap/start-elasticsearch.ps1
+++ bootstrap/start-elasticsearch.ps1
@@ -14,4 +14,4 @@ sudo chown -R 1000:1000 ~/srv/$service_name
 sudo chmod -R 744 ~/srv/$service_name
 
 docker rm -f $service_name
-docker run -d --name $service_name --network $docker_network -p 9200:9200 -p 9300:9300 -v $HOME/srv/$service_name/data:/home/app_user/data -v $HOME/srv/$service_name/logs:/home/app_user/logs:rw $docker_image
+docker run -d --name $service_name --network $docker_network -v $HOME/srv/$service_name/data:/home/app_user/data -v $HOME/srv/$service_name/logs:/home/app_user/logs:rw $docker_image
