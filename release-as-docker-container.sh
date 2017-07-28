9332794b74725622cb76e8110a3873228a4825a7
Alex <alexander.orlov@loxal.net>
2017-07-28 14:33:41 +0200

    harden deployment script

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -2,8 +2,7 @@
 
 ./gradlew clean build
 
-echo `ls build/libs/*.jar | sed "s/.*\/\(.\+\)-.*\.jar/\1/"`
-DOCKER_IMAGE_NAME=`ls build/libs/*.jar | sed "s/.*\/\(.\+\)-.*\.jar/\1/"`
+DOCKER_IMAGE_NAME=`ls build/libs/*.jar | sed "s/.*\/\(.\+\).*\.jar/\1/"`
 DOCKER_TAG=latest
 
 docker build --tag intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG .
@@ -13,4 +12,11 @@ docker run -d --name $DOCKER_IMAGE_NAME \
     -v ~/srv/$DOCKER_IMAGE_NAME:/data \
     intrafind/$DOCKER_IMAGE_NAME:$DOCKER_TAG
 
-docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
+danglingImages=$(docker images -f 'dangling=true' -q)
+if [ -z "$danglingImages" ]
+then
+    echo 'there are no dangling Docker images'
+else
+    docker rmi -f $danglingImages # cleanup, GC for dangling images
+fi
+
