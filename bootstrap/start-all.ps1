1844839d25939123055062250a4e76afb193cc36
Alex <alexander.orlov@loxal.net>
2017-07-28 14:53:34 +0200

    introduce further step of bootstrapping automation

---
+++ bootstrap/start-all.ps1
@@ -0,0 +1,2 @@
+iex start-elasticsearch.ps1
+& start-search-service.ps1
--- bootstrap/start-search-service.ps1
+++ bootstrap/start-search-service.ps1
@@ -13,4 +13,5 @@ docker load -i "~/tmp/$service_name.tar"
 sudo chown -R 1000:1000 ~/srv/$service_name
 sudo chmod -R 744 ~/srv/$service_name
 
-docker run --rm --name $service_name --network $docker_network -p 9605:9605 -v $HOME/srv/$service_name/logs:/home/app_user/logs $docker_image
+docker rm -f $service_name
+docker run -d --name $service_name --network $docker_network -p 9605:9605 -v $HOME/srv/$service_name/logs:/home/app_user/logs $docker_image
