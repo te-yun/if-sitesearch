15f59d933dd2b7fde9b623280e45316d53549e7f
Alex <alexander.orlov@loxal.net>
2017-07-28 14:16:18 +0200

    harden bootstrap script

--- bootstrap/start-elasticsearch.ps1
+++ bootstrap/start-elasticsearch.ps1
@@ -10,8 +10,7 @@ docker network create $docker_network
 
 docker load -i "~/tmp/$service_name.tar"
 
-
 sudo chown -R 1000:1000 ~/srv/$service_name
 sudo chmod -R 744 ~/srv/$service_name
 
-docker run --rm --name $service_name --network $docker_network -p 9200:9200 -p 9300:9300 -v $HOME/srv/$service_name/data:/home/app_user/data -v $HOME/srv/$service_name/logs:/home/app_user/logs:rw $docker_image
+docker run -d --name $service_name --network $docker_network -p 9200:9200 -p 9300:9300 -v $HOME/srv/$service_name/data:/home/app_user/data -v $HOME/srv/$service_name/logs:/home/app_user/logs:rw $docker_image
