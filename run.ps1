2759bbfedd7102601e890b5bd87e440facfc0e10
Alex <alexander.orlov@loxal.net>
2017-07-31 10:34:49 +0200

    fix run script

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 Param([string] $suffix_args)
 
 Function Main() {
-    ./gradlew run --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args # add "--debug-jvm" to attach debugger
+    ./gradlew bootRun --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args # add "--debug-jvm" to attach debugger
 }
 Main
