e21e0ee0b76eccc596480d917b6cbe6e2b0b7e80
Alex <alexander.orlov@loxal.net>
2017-08-01 17:03:17 +0200

    make execution scripts more flexible

--- run.ps1
+++ run.ps1
@@ -1,6 +1,7 @@
 Param([string] $suffix_args)
 
+# add "--debug-jvm" to attach debugger
 Function Main() {
-    ./gradlew bootRun --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args # add "--debug-jvm" to attach debugger
+    ./gradlew bootRun --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args
 }
 Main
--- test.ps1
+++ test.ps1
@@ -1,4 +1,7 @@
+Param([string] $suffix_args)
+
+# add "--debug-jvm" to attach debugger
 Function Main {
-    ./gradlew clean test --parallel --no-scan --no-rebuild --info # --continuous --build-cache 
+    ./gradlew test --parallel --no-scan --no-rebuild --continuous --build-cache $suffix_args
 }
 Main
