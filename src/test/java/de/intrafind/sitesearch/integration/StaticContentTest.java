1264dd47d8724a2e32bb975c1530df5daa36d81f
Alex <alexander.orlov@loxal.net>
2017-08-01 15:15:52 +0200

    cover static content with tests

---
+++ src/test/java/de/intrafind/sitesearch/integration/StaticContentTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.intrafind.sitesearch.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaticContentTest {
+    @Autowired
+    private TestRestTemplate caller;
+
+    @Test
+    public void indexPage() throws Exception {
+        final ResponseEntity<String> indexPage = caller.getForEntity("/index.html", String.class);
+
+        assertEquals(HttpStatus.OK, indexPage.getStatusCode());
+        assertTrue(indexPage.getBody().contains("API Specification"));
+    }
+
+    @Test
+    public void searchbarPage() throws Exception {
+        final ResponseEntity<String> indexPage = caller.getForEntity("/searchbar/searchbar.html", String.class);
+
+        assertEquals(HttpStatus.OK, indexPage.getStatusCode());
+        assertTrue(indexPage.getBody().contains("js/app.js"));
+    }
+
+    @Test
+    public void searchbarConfiguration() throws Exception {
+        final ResponseEntity<String> indexPage = caller.getForEntity("/searchbar/data/config.json", String.class);
+
+        assertEquals(HttpStatus.OK, indexPage.getStatusCode());
+        assertTrue(indexPage.getBody().contains("\"baseUrl\": \"http://sitesearch.cloud/\","));
+    }
+
+    @Test
+    public void searchbarAppJs() throws Exception {
+        final ResponseEntity<String> indexPage = caller.getForEntity("/searchbar/js/app.js", String.class);
+
+        assertEquals(HttpStatus.OK, indexPage.getStatusCode());
+        assertTrue(indexPage.getBody().contains("if-app-searchbar"));
+    }
+}
+
+
