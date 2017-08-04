0cad8a7862af1e5a769bd438e189ce5974935b4b
Alex <alexander.orlov@loxal.net>
2017-08-04 16:04:46 +0200

    fix CI

--- src/test/java/de/intrafind/sitesearch/integration/SiteTest.java
+++ src/test/java/de/intrafind/sitesearch/integration/SiteTest.java
@@ -147,6 +147,7 @@ public class SiteTest {
         assertEquals(25, tenantInfo.getSuccessfullyIndexed().intValue());
         assertTrue(tenantInfo.getFailed().isEmpty());
 
+        Thread.sleep(9000);
         LOG.info("tenantId: " + tenantInfo.getTenantId());
         final ResponseEntity<Hits> hitFromTenant = caller.exchange(SearchController.ENDPOINT + "?query=Knowledge&tenantId=" + tenantInfo.getTenantId(), HttpMethod.GET, null, Hits.class);
         assertEquals(HttpStatus.OK, hitFromTenant.getStatusCode());
