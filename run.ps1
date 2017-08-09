4acab11a35d5d9801e0184494615a37f4aa6b925
Alex <alexander.orlov@loxal.net>
2017-08-09 23:19:48 +0200

    make startup & run scripts more flexible & resilient

--- run.ps1
+++ run.ps1
@@ -1,7 +1,16 @@
-Param([string] $suffix_args)
+#Param([string] $suffix_args)
 
-# add "--debug-jvm" to attach debugger
-Function Main() {
-    ./gradlew bootRun --no-scan --parallel --no-rebuild --continuous --build-cache $suffix_args
+try {
+    echo "suffix_args: $suffix_args"
+    Write-Host "args: $args"
+    
+    # add "--debug-jvm" to attach debugger
+    Function Main() {
+        ./gradlew bootRun --no-scan --parallel --no-rebuild --continuous --build-cache $args
+    }
+    Main
+} Finally {
+    $hangingJavaProcessToStop = [regex]::match((jps), "(\d+)\ Application").Groups[1].Value
+    Stop-Process -Id $hangingJavaProcessToStop
+    Write-Host "Gracefully killed hanging process: $hangingJavaProcessToStop"
 }
-Main
--- test.ps1
+++ test.ps1
@@ -1,9 +1,11 @@
-Param([string] $suffix_args)
+#Param([string] $suffix_args)
 
 # add "--debug-jvm" to attach debugger
 Function Main {
     Write-Host "suffix_args: $suffix_args"
-    ./gradlew test --no-scan --parallel --no-rebuild --continuous --build-cache $suffix_args
-#    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --tests *SiteTest.importFeedAndUpdate $suffix_args
+    Write-Host "suffix_args: $args"
+    ./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
+#    ./gradlew test --no-scan --parallel --no-rebuild --continuous --build-cache $args
+#    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --tests *SiteTest.importFeedAndUpdate $args
 }
 Main
