e89b918a81c6f5b8d392ad605998dedc7da441eb
Alex <alexander.orlov@loxal.net>
2017-08-10 03:05:39 +0200

    fix scripts

--- run.ps1
+++ run.ps1
@@ -1,14 +1,9 @@
 #Param([string] $suffix_args)
 
 try {
-    echo "suffix_args: $suffix_args"
-    Write-Host "args: $args"
-    
     # add "--debug-jvm" to attach debugger
-    Function Main() {
-        ./gradlew bootRun --no-scan --parallel --no-rebuild --continuous --build-cache $args
-    }
-    Main
+    Write-Host "args: $args"
+    ./gradlew bootRun --no-scan --parallel --no-rebuild --continuous --build-cache $args
 } Finally {
     $hangingJavaProcessToStop = [regex]::match((jps), "(\d+)\ Application").Groups[1].Value
     Stop-Process -Id $hangingJavaProcessToStop
--- test.ps1
+++ test.ps1
@@ -4,8 +4,8 @@
 #function main {
     Write-Host "suffix_args: $suffix_args"
     Write-Host "args: $args"
-    ./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
+#    ./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
 #    ./gradlew test --no-scan --parallel --no-rebuild --continuous --build-cache $args
-#    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --tests *SiteTest.importFeedAndUpdate $args
+    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --tests *SiteTest.importFeedAndUpdate $args
 #}
 #main
