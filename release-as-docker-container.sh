8c895c806e384693841beba497c198d12c13b466
Alex <alexander.orlov@loxal.net>
2017-08-16 18:22:31 +0200

    enable fetching feeds via SSL

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -25,7 +25,7 @@ fi
 ### Redirect HTTP to HTTPS traffic
 cd docker-nginx-https-redirect
 docker build --tag intrafind/redirect-https:latest .
-docker rm -f redirect-https
+#docker rm -f redirect-https
 docker run -d --name redirect-https \
     -p 80:443 \
     intrafind/redirect-https:latest
