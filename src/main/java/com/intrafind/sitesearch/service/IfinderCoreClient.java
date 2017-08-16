55d99103438e305645aa3d56576cf493a37971e8
Alex <alexander.orlov@loxal.net>
2017-08-16 11:44:10 +0200

    fix classpath location

--- src/main/java/com/intrafind/sitesearch/service/IfinderCoreClient.java
+++ src/main/java/com/intrafind/sitesearch/service/IfinderCoreClient.java
@@ -94,11 +94,12 @@ public enum IfinderCoreClient {
 
 
     private static void initSSL() {
-        setEnv("javax.net.ssl.trustStore", "client-truststore.ks");
-        setEnv("javax.net.ssl.trustStorePassword", "changeit");
+//        setEnv("javax.net.ssl.trustStore", "client-truststore.ks");
+//        setEnv("javax.net.ssl.trustStorePassword", "changeit");
 
-        setEnv("javax.net.ssl.keyStore", "client-keystore.ks"); // increased security with client authentication
-        setEnv("javax.net.ssl.keyStorePassword", "changeit");
+//        setEnv("javax.net.ssl.keyStore", "classpath:keystore.jks"); // increased security with client authentication
+        setEnv("javax.net.ssl.keyStore", "/home/alexander_orlov/buildAgent/work/e92715f0513d41ad/src/main/resources/keystore.jks"); // increased security with client authentication
+        setEnv("javax.net.ssl.keyStorePassword", "7zCMZiSN2X2k0lK6Abovg4C3uFkpRwaJLwX87jpFKmc=");
     }
 
     private static void initUrlAuthentication() {
