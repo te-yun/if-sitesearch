6609f3022124f2f1031d704b2626b7a7cb43ae67
Alex <alexander.orlov@loxal.net>
2017-08-23 18:50:03 +0200

    adjust blue/green deployment script

--- switch-release.ps1
+++ switch-release.ps1
@@ -10,11 +10,11 @@ function isBlueUp() {
 }
 
 if(isBlueUp){
-    write-host blue is up
+    write-host blue is up and will be removed
     docker run -d --name ${docker_image_name}-green -p 3443:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/data intrafind/${docker_image_name}:${docker_tag}
     docker rm -f ${docker_image_name}-blue
 } else {
-    write-host blue is down
+    write-host blue is down and will be removed
     docker run -d --name ${docker_image_name}-blue -p 4443:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/data intrafind/${docker_image_name}:${docker_tag}
     docker rm -f ${docker_image_name}-green
 }
