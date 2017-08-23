81b5c3aad8915db29ae8c2dc0383236380a2058c
Alex <alexander.orlov@loxal.net>
2017-08-23 21:28:08 +0200

    do not build https redirect container every time

--- release.ps1
+++ release.ps1
@@ -11,10 +11,12 @@ docker rm -f ${DOCKER_IMAGE_NAME}
 docker run -d --name ${DOCKER_IMAGE_NAME} -p 443:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/data intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG}
 
 # Redirect HTTP to HTTPS traffic
-cd docker-nginx-https-redirect
-docker build --tag intrafind/redirect-https:latest .
+$httpsRedirect = "http-to-https-redirect"
+cd $httpsRedirect
+docker build --tag intrafind/${httpsRedirect}:latest .
+docker rm -f $httpsRedirect
 docker rm -f redirect-https
-docker run -d --name redirect-https -p 80:80 intrafind/redirect-https:latest
+docker run -d --name $httpsRedirect -p 80:80 loxal/${httpsRedirect}:1.0.0 
 
 cd ..
 ./switch-release.ps1
