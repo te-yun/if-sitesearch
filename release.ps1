a9e7b049a007ab455276a7584eac2718a77672f4
Alex <alexander.orlov@loxal.net>
2017-08-23 18:38:08 +0200

    update searchbar gadget

--- release.ps1
+++ release.ps1
@@ -10,14 +10,6 @@ docker build --tag intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG} .
 docker rm -f ${DOCKER_IMAGE_NAME}
 docker run -d --name ${DOCKER_IMAGE_NAME} -p 443:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/data intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG}
 
-$danglingImages = $(docker images -f "dangling=true" -q)
-
-if ([string]::IsNullOrEmpty($danglingImages)){
-    "There are no dangling Docker images"
-} else {
-    docker rmi -f $danglingImages # cleanup, GC for dangling images
-}
-
 # Redirect HTTP to HTTPS traffic
 cd docker-nginx-https-redirect
 docker build --tag intrafind/redirect-https:latest .
@@ -26,3 +18,10 @@ docker run -d --name redirect-https -p 80:80 intrafind/redirect-https:latest
 
 cd ..
 ./switch-release.ps1
+
+$danglingImages = $(docker images -f "dangling=true" -q)
+if ([string]::IsNullOrEmpty($danglingImages)){
+    "There are no dangling Docker images"
+} else {
+    docker rmi -f $danglingImages # cleanup, GC for dangling images
+}
--- switch-release.ps1
+++ switch-release.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env powershell
+
 $docker_image_name = "if-sitesearch"
 $docker_tag = "latest"
 
