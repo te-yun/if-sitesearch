70a67dd8347c81b9d37450eb1d78e2896b4f8770
Alex <alexander.orlov@loxal.net>
2017-09-08 18:31:21 +0200

    update routes; cleanup;

--- run.ps1
+++ run.ps1
@@ -2,7 +2,6 @@
 
 try {
     # add "--debug-jvm" to attach debugger
-    Write-Host "args: $args"
 #    $Env:SPRING_CONFIG_NAME = "application"
 #    Write-Host "SPRING_CONFIG_NAME $Env:SPRING_CONFIG_NAME"
     ./gradlew bootRun --no-scan --parallel --no-rebuild --continuous --build-cache $args --continue
--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env powershell
 
 # add "--debug-jvm" to attach debugger
-Write-Host "args: $args"
-#    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --continuous --continue $args
-./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
+#    $Env:SPRING_CONFIG_NAME = "application, local"
+    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --continuous --continue $args
+#./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
