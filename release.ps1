44ffa4baa9bdfcc6137556434b5441b739a7b793
Alex <alexander.orlov@loxal.net>
2017-09-11 01:06:12 +0200

    revert routing

--- release.ps1
+++ release.ps1
@@ -13,14 +13,15 @@ sudo chown -R 1000:1000 ~/srv/${DOCKER_IMAGE_NAME} # make it a svc_usr' director
 # TODO enable b/g deployment, or at least, reduce probability of failure 
 docker build --tag intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG} .
 docker rm -f ${DOCKER_IMAGE_NAME}
-docker run -d --name ${DOCKER_IMAGE_NAME} -p 8001:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/home/svc_usr/data intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG}
+docker run -d --name ${DOCKER_IMAGE_NAME} -p 443:8001 --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH  -v ~/srv/${DOCKER_IMAGE_NAME}:/home/svc_usr/data intrafind/${DOCKER_IMAGE_NAME}:${DOCKER_TAG}
 
 # Redirect HTTP to HTTPS traffic
 $httpsRedirect = "http-to-https-redirect"
 cd $httpsRedirect
 docker build --tag intrafind/${httpsRedirect}:latest .
 docker rm -f $httpsRedirect
-docker run -d --name $httpsRedirect -p 80:80 -p 443:443 intrafind/${httpsRedirect}:latest
+#docker run -d --name $httpsRedirect -p 80:80 -p 443:443 intrafind/${httpsRedirect}:latest
+docker run -d --name $httpsRedirect -p 80:80 intrafind/${httpsRedirect}:latest
 
 cd ..
 ./switch-release.ps1
