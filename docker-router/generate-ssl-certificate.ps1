68a28a7a2ada6699c04d8e66c522228ef6682fdb
Alex <alexander.orlov@loxal.net>
2017-09-18 22:43:02 +0200

    adjust routing

--- docker-router/generate-ssl-certificate.ps1
+++ docker-router/generate-ssl-certificate.ps1
@@ -9,7 +9,7 @@ sudo apt install certbot
 sudo certbot certonly
 
 #sudo certbot --webroot certonly -w /var/www/letsencrypt --expand --domains "sitesearch.cloud, www.sitesearch.cloud, dev.sitesearch.cloud, api.sitesearch.cloud, ci.sitesearch.cloud"
-sudo certbot certonly --expand --domains "sitesearch.cloud, www.sitesearch.cloud, dev.sitesearch.cloud, api.sitesearch.cloud, ci.sitesearch.cloud"
+sudo certbot certonly --expand --domains "sitesearch.cloud, www.sitesearch.cloud, dev.sitesearch.cloud, api.sitesearch.cloud, ci.sitesearch.cloud, main.sitesearch.cloud, test.sitesearch.cloud"
 
 sudo openssl pkcs12 -export -in /etc/letsencrypt/live/sitesearch.cloud/fullchain.pem -inkey /etc/letsencrypt/live/sitesearch.cloud/privkey.pem -out pkcs.p12 -name alias
 keytool -importkeystore -destkeystore keystore.jks -srcstoretype PKCS12 -srckeystore pkcs.p12 -alias alias
