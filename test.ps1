498cc15cd783fc818d700fc791273868158d03d8
Alex <alexander.orlov@loxal.net>
2017-09-21 17:05:33 +0200

    revert to older Gradle version

--- test.ps1
+++ test.ps1
@@ -2,5 +2,8 @@
 
 # add "--debug-jvm" to attach debugger
 #    $Env:SPRING_CONFIG_NAME = "application, local"
-    ./gradlew test --no-scan --parallel --no-rebuild --build-cache --continuous --continue $args
+    ./gradlew test `
+    --no-scan --parallel --no-rebuild `
+    --build-cache --continuous --continue `
+    $args
 #./gradlew test --no-scan --parallel --no-rebuild --build-cache $args
