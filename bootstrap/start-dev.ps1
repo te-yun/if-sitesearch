aa40d7ef709d051b356ad54513d7d0b4e9e2521e
Alex <alexander.orlov@loxal.net>
2017-09-29 14:53:44 +0200

    fix CI

--- bootstrap/start-dev.ps1
+++ bootstrap/start-dev.ps1
@@ -5,10 +5,10 @@ docker start teamcity-server
 ~/buildAgent/bin/agent.sh start
 
 #core
+docker start router
 docker start elasticsearch
 docker start sitesearch-search-service
 docker start if-sitesearch
-docker start http-to-https-redirect
 
 # supplementary
 docker start if-sitesearch-green
