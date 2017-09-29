82e1e04145fc0e6acd6baf1f4811dc7d8f1858c0
Alex <alexander.orlov@loxal.net>
2017-09-29 15:28:42 +0200

    establish *.online redirect

--- bootstrap/start-dev.ps1
+++ bootstrap/start-dev.ps1
@@ -4,14 +4,13 @@
 docker start teamcity-server
 ~/buildAgent/bin/agent.sh start
 
-#core
-docker start router
 docker start elasticsearch
 docker start sitesearch-search-service
-docker start if-sitesearch
 
-# supplementary
+docker start if-sitesearch
 docker start if-sitesearch-green
 docker start if-sitesearch-blue
+
+docker start router
 
 
