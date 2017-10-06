5c693bb10199d3a49ab56bd48f9e0c43bd7b66ae
Alex <alexander.orlov@loxal.net>
2017-10-06 19:09:21 +0200

    adjust nginx script

--- serve-js-app.ps1
+++ serve-js-app.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 
 docker rm -f if-sitesearch-js-app
-docker run -it --name if-sitesearch-js-app `
+docker run -d --name if-sitesearch-js-app `
     -p 8002:80 `
     -v $PWD/service/src/main/resources/static:/usr/share/nginx/html:ro `
     nginx:alpine
