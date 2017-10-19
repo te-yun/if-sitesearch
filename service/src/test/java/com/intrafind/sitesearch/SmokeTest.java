5520f34487be701a4a649d729a16e737ea6685be
Alex <alexander.orlov@loxal.net>
2017-10-19 13:40:21 +0200

    introduce http library that can handle CORS unlike Spring Boot's default

--- service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
+++ service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
@@ -16,10 +16,14 @@
 
 package com.intrafind.sitesearch;
 
-import com.intrafind.sitesearch.dto.Autocomplete;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intrafind.sitesearch.dto.FoundPage;
 import com.intrafind.sitesearch.dto.Hits;
 import com.intrafind.sitesearch.integration.SearchTest;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -27,12 +31,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.MultiValueMap;
 
 import java.net.URI;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -97,86 +104,70 @@ public class SmokeTest {
         assertTrue(response.getBody().contains("<title>Site Search from IntraFind Software AG</title>"));
     }
 
-    private void assureCorsHeaders(ResponseEntity<?> response) {
-//        curl -X GET   'https://api.sitesearch.cloud/search?query=ifInder&siteId=5f2b9c2e-6071-4f30-8972-7781fac73726' -H 'origin: http://localhost:8001' -v
-//        < Access-Control-Allow-Origin: http://localhost:8001
-//        < Vary: Origin
-//        < Access-Control-Allow-Credentials: true
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final OkHttpClient HTTP_CLIENT = new OkHttpClient.Builder().build();
+    private static final Map<String, String> CORS_TRIGGERING_REQUEST_HEADER = new HashMap<String, String>() {
+        {
+            put("origin", "https://example.com");
+        }
+    };
 
-//        assertEquals(0, response.getHeaders().getAccessControlAllowHeaders().size());
-//        assertEquals(0, response.getHeaders().get("access-control-allow-origin").size());
-//        assertTrue(response.getHeaders().getAccessControlAllowCredentials());
-
-        assertTrue(response.getHeaders().containsKey("access-control-allow-credentials"));
-        assertTrue(response.getHeaders().getAccessControlAllowCredentials());
-        
-        response.getHeaders().forEach((headerName, headerValues) -> {
-            LOG.warn("headerName>>: " + headerName);
-            if (headerName.equalsIgnoreCase("access-control-allow-origin")) {
-                throw new RuntimeException("Found header: Access-Control-Allow-Origin");
-            }
-//            if (headerName.equalsIgnoreCase("Access-Control-Allow-Origin")) {
-//                throw new RuntimeException("Found header: Access-Control-Allow-Origin");
-//            }
-//            if (headerName.equalsIgnoreCase("access-control-allow-credentials")) {
-////                throw new RuntimeException("Found header: Access-Control-Allow-Credentials");
-//            }
-            headerValues.forEach(headerValue -> {
-                LOG.warn("headerValue>>>: " + headerValue);
-            });
-        });
+    private void assureCorsHeaders(Headers headers, int byteCount) {
+        assertEquals(byteCount, headers.byteCount());
+        assertEquals("https://example.com", headers.get("access-control-allow-origin"));
+        assertEquals("true", headers.get("access-control-allow-credentials"));
     }
 
     @Test
     public void apiFrontpageContent() throws Exception {
-        final ResponseEntity<String> response = caller.exchange(
-                "https://api.sitesearch.cloud",
-                HttpMethod.GET,
-                HttpEntity.EMPTY,
-                String.class
-        );
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertTrue(response.getBody().contains("<title>Simple Site Search</title>"));
+        Request request = new Request.Builder()
+                .url("https://api.sitesearch.cloud")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build();
+        final Response response = HTTP_CLIENT.newCall(request).execute();
+        assertEquals(HttpStatus.OK.value(), response.code());
+        assertNotNull(response.body());
+        assertTrue(response.body().string().contains("<title>Simple Site Search</title>"));
 
-        assureCorsHeaders(response);
+        assertEquals(HttpStatus.OK.value(), response.code());
+        assureCorsHeaders(response.headers(), 495);
     }
 
     @Test
     public void search() throws Exception {
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        headers.put("origin", Collections.singletonList("https://intrafind.de"));
-        headers.put("User-Agent", Collections.singletonList("curl/7.47.0"));
-        headers.put("Host", Collections.singletonList("api.sitesearch.cloud"));
-        headers.put("Accept", Collections.singletonList("*/*"));
-        final HttpEntity httpEntity = new HttpEntity(headers);
-        final ResponseEntity<Hits> searchResults = caller.exchange(
-                "https://api.sitesearch.cloud/search?query=Knowledge&siteId=" + SearchTest.SEARCH_SITE_ID,
-                HttpMethod.GET,
-                httpEntity,
-                Hits.class
-        );
+        Request request = new Request.Builder()
+                .url("https://api.sitesearch.cloud/search?query=Knowledge&siteId=" + SearchTest.SEARCH_SITE_ID)
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build();
+        final Response response = HTTP_CLIENT.newCall(request).execute();
 
-        assertEquals(HttpStatus.OK, searchResults.getStatusCode());
-        assertNotNull(searchResults.getBody());
-        assertEquals("Knowledge", searchResults.getBody().getQuery());
-        assertEquals(1, searchResults.getBody().getResults().size());
-        FoundPage found = searchResults.getBody().getResults().get(0);
+        assertEquals(HttpStatus.OK.value(), response.code());
+        assertNotNull(response.body());
+        Hits result = MAPPER.readValue(response.body().bytes(), Hits.class);
+        assertEquals("Knowledge", result.getQuery());
+        assertEquals(1, result.getResults().size());
+        FoundPage found = result.getResults().get(0);
         assertEquals("Wie die Semantische Suche vom <span class=\"if-teaser-highlight\">Knowledge</span> Graph profitiert", found.getTitle());
         assertEquals("http:&#x2F;&#x2F;intrafind.de&#x2F;blog&#x2F;wie-die-semantische-suche-vom-<span class=\"if-teaser-highlight\">knowledge</span>-graph-profitiert", found.getUrl());
         assertEquals("http://intrafind.de/blog/wie-die-semantische-suche-vom-knowledge-graph-profitiert", found.getUrlRaw());
         assertTrue(found.getBody().startsWith("&lt;p&gt;Der <span class=\"if-teaser-highlight\">Knowledge</span> Graph ist vielen Nutzern bereits durch Google oder Facebook bekannt. Aber auch"));
 
-        assureCorsHeaders(searchResults);
+        assureCorsHeaders(response.headers(), 429);
     }
 
     @Test
     public void autocomplete() throws Exception {
-        final ResponseEntity<Autocomplete> actual = caller.getForEntity("https://api.sitesearch.cloud/autocomplete?query=Knowledge&siteId=" + SearchTest.SEARCH_SITE_ID, Autocomplete.class);
+        Request request = new Request.Builder()
+                .url("https://api.sitesearch.cloud/autocomplete?query=Knowledge&siteId=" + SearchTest.SEARCH_SITE_ID)
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build();
+        final Response response = HTTP_CLIENT.newCall(request).execute();
 
-        assertEquals(HttpStatus.NOT_FOUND, actual.getStatusCode()); // actually 200, should be returned but due to a bug this is not the case yet
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.code()); // actually 200, should be returned but due to a bug this is not the case yet
+//        Hits result = MAPPER.readValue(response.body().bytes(), Autocomplete.class);
 //        assertNotNull(actual.getBody());
 //        assertEquals(1, actual.getBody().getResults().size());
 //        assertEquals("knowledge graph", actual.getBody().getResults().get(0));
-        assureCorsHeaders(actual);
+        assureCorsHeaders(response.headers(), 374);
     }
 }
