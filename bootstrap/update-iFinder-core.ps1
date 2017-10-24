c4c81d7263ca550e2bb76e9e7e0c1b87569cfda6
Alex <alexander.orlov@loxal.net>
2017-10-24 17:26:47 +0200

    update scripts for iFinder Core

--- bootstrap/update-iFinder-core.ps1
+++ bootstrap/update-iFinder-core.ps1
@@ -4,5 +4,5 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 
-iex ./start-sitesearch-elasticsearch.ps1
+./start-sitesearch-elasticsearch.ps1
 & ./start-sitesearch-search-service.ps1
