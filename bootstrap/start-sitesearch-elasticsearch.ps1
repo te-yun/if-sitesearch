073ce7a9fc708e9a90b0f9253bd03af0672448e4
Alex <alexander.orlov@loxal.net>
2017-10-24 16:23:38 +0200

    update scripts for iFinder Core

--- bootstrap/start-sitesearch-elasticsearch.ps1
+++ bootstrap/start-sitesearch-elasticsearch.ps1
@@ -7,17 +7,18 @@ $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 $docker_network = "sitesearch"
 $service_name = "sitesearch-elasticsearch"
 $docker_image = "intrafind/$service_name"
+$persistence_dir = "srv1"
 
 docker network create $docker_network
 
 docker load -i "~/tmp/$service_name.tar"
 
-sudo chown -R 1000:1000 ~/srv/$service_name
-sudo chmod -R 744 ~/srv/$service_name
+sudo chown -R 1000:1000 $HOME/$persistence_dir/$service_name
+sudo chmod -R 744 $HOME/$persistence_dir/$service_name
 
 docker rm -f $service_name
 docker run -d --name $service_name `
     --network $docker_network `
-    -v $HOME/srv/$service_name/data:/home/app_user/data `
-    -v $HOME/srv/$service_name/logs:/home/app_user/logs:rw `
+    -v $HOME/$persistence_dir/$service_name/data:/home/app_user/data `
+    -v $HOME/$persistence_dir/$service_name/logs:/home/app_user/logs:rw `
     $docker_image
--- bootstrap/start-sitesearch-search-service.ps1
+++ bootstrap/start-sitesearch-search-service.ps1
@@ -7,16 +7,17 @@ $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 $docker_network = "sitesearch"
 $service_name = "sitesearch-search-service"
 $docker_image = "intrafind/$service_name"
+$persistence_dir = "srv1"
 
 docker network create $docker_network
 
 docker load -i "~/tmp/$service_name.tar"
 
-sudo chown -R 1000:1000 ~/srv/$service_name
-sudo chmod -R 744 ~/srv/$service_name
+sudo chown -R 1000:1000 $HOME/$persistence_dir/$service_name
+sudo chmod -R 744 $HOME/$persistence_dir/$service_name
 
 docker rm -f $service_name
 docker run -d --name $service_name --network $docker_network `
     -p 9605:9605 `
-    -v $HOME/srv/$service_name/logs:/home/app_user/logs `
+    -v $HOME/$persistence_dir/$service_name/logs:/home/app_user/logs `
     $docker_image
