de40f43c1848214fc695f5d936ae56fae4f59890
Alex <alexander.orlov@loxal.net>
2017-11-23 15:41:23 +0100

    cleanup

--- client/src/main/kotlin/com/intrafind/sitesearch/client/main.kt
+++ client/src/main/kotlin/com/intrafind/sitesearch/client/main.kt
@@ -16,17 +16,16 @@
 
 package com.intrafind.sitesearch.client
 
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLInputElement
-import org.w3c.dom.HTMLScriptElement
-import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.*
 import org.w3c.dom.events.Event
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.dom.clear
 
 fun main(args: Array<String>) {
     window.addEventListener("DOMContentLoaded", {
+        init()
     })
 }
 
@@ -52,16 +51,35 @@ fun triggerFirstUsageOwnership() {
     xhr.send()
 }
 
+private val loginLink = document.getElementById("loginLink") as HTMLAnchorElement
+fun init() {
+    loginLink.text = " Login"
+    if (window.location.hostname.equals("localhost")) {
+        loginLink.href = "http://${window.location.host}/login?redirect_uri=http://${window.location.host}/login"
+    } else {
+        loginLink.href = "https://api.sitesearch.cloud/login?redirect_uri=https://api.sitesearch.cloud/login"
+    }
+
+    val xhr = XMLHttpRequest()
+    xhr.open("GET", "/user")
+    xhr.onload = {
+        showUser(xhr)
+    }
+    xhr.send()
+}
+
 fun showInitCode() {
     val siteIdContainer = document.getElementById("siteId") as HTMLInputElement
     val enterpriseSearchbar = document.getElementById("sitesearch-searchbar") as HTMLDivElement
     val finderInit = document.getElementById("sitesearch-page-finder-init") as HTMLScriptElement
     val finderContainer = document.getElementById("page-finder") as HTMLDivElement
     val finderVariant = document.getElementById("finder-variant") as HTMLInputElement
-    val searchbarInputField = document.getElementById("ifs-sb-searchfield") as HTMLInputElement
+//    val searchbarInputField = document.getElementById("ifs-sb-searchfield") as HTMLInputElement
     val searchbarVariant = document.getElementById("searchbar-variant") as HTMLInputElement
     val integrationCode = document.getElementById("integration-code") as HTMLTextAreaElement
-    searchbarInputField.placeholder = "Search for \"knowledge\""
+    window.addEventListener("DOMContentLoaded", { console.warn(1) })
+    document.addEventListener("DOMContentLoaded", { console.warn(2) })
+//    searchbarInputField.placeholder = "Search for \"knowledge\""
     val enterpriseSearchbarCode = enterpriseSearchbar.outerHTML
             .replace("searchbar-config/sitesearch-config.json", "https://api.sitesearch.cloud/searchbar-config/sitesearch-config.json")
     integrationCode.value = enterpriseSearchbarCode
@@ -100,3 +118,103 @@ fun showInitCode() {
         }
     })
 }
+
+private fun showUser(xhr: XMLHttpRequest) {
+    if (xhr.status.equals(200) && xhr.responseText.isEmpty()) {
+        loginLink.text = " Login"
+    } else
+        if (xhr.status.equals(200) && xhr.response != null) {
+            showAssignments()
+            val user = JSON.parse<dynamic>(xhr.responseText)
+            (document.getElementById("assignmentController") as HTMLDivElement).style.display = "block"
+            loginLink.textContent = " Logout: ${user.userAuthentication.details.name}"
+            loginLink.title = user.userAuthentication.details.id
+            loginLink.className = "fa fa-sign-out"
+            loginLink.href = "/logout"
+            if (document.getElementById("company") != null) {
+                (document.getElementById("company") as HTMLInputElement).value = user.userAuthentication.details.company
+                (document.getElementById("contactEmail") as HTMLInputElement).value = user.userAuthentication.details.email
+            }
+        }
+}
+
+@JsName("assignSite")
+private fun assignSite() {
+    val xhr = XMLHttpRequest()
+    xhr.open("GET", "/user")
+    xhr.onload = {
+        if (xhr.status.equals(200) && xhr.response != null) {
+            val user = JSON.parse<dynamic>(xhr.responseText)
+            val xhr = XMLHttpRequest()
+            xhr.open("PUT", "/assignments/sites/"
+                    + (document.getElementById("siteId") as HTMLInputElement).value + "?siteSecret="
+                    + (document.getElementById("siteSecret") as HTMLInputElement).value
+                    + "&siteName=" + (document.getElementById("siteName") as HTMLInputElement).value)
+            xhr.setRequestHeader("content-type", "application/json")
+            xhr.onload = {
+                showAssignments()
+            }
+
+            val tenantSiteAssignment: dynamic = Any()
+            tenantSiteAssignment.authProviderId = user.userAuthentication.details.id
+            tenantSiteAssignment.authProvider = "github"
+            tenantSiteAssignment.authProviderToken = user.details.tokenValue
+            tenantSiteAssignment.contactEmail = (document.getElementById("contactEmail") as HTMLInputElement).value
+            tenantSiteAssignment.company = (document.getElementById("company") as HTMLInputElement).value
+
+//            xhr.send(JSON.stringify("""
+//                "authProviderId": "${user.userAuthentication.details.id}",
+//                "authProvider": "github",
+//                "authProviderToken": "${user.details.tokenValue}",
+//                "contactEmail": "${(document.getElementById("contactEmail") as HTMLInputElement).value}",
+//                "company": "${(document.getElementById("company") as HTMLInputElement).value}"
+//            """))
+            xhr.send(JSON.stringify(tenantSiteAssignment))
+        }
+    }
+    xhr.send()
+}
+
+private fun showAssignments() {
+    val xhr = XMLHttpRequest()
+    xhr.open("GET", "/user")
+    xhr.onload = {
+        if (xhr.status.equals(200) && xhr.response != null) {
+            val user = JSON.parse<dynamic>(xhr.responseText)
+            val xhr = XMLHttpRequest()
+            xhr.open("GET", "/authentication-providers/github/" + user.userAuthentication.details.id
+                    + "?accessToken=" + user.details.tokenValue)
+            xhr.setRequestHeader("content-type", "application/json")
+            xhr.onload = {
+                if (xhr.status.equals(200) && xhr.response != null) {
+                    document.getElementById("assignments")?.clear()
+                    document.getElementById("assignmentsContainer")?.setAttribute("style", "display: block;")
+                    val assignments = JSON.parse<dynamic>(xhr.responseText)
+                    assignments.tenants.forEach({ tenant ->
+                        val tenantCompany = document.getElementById("companyName") as HTMLElement
+                        val tenantContactEmail = document.getElementById("companyContact") as HTMLElement
+                        tenantCompany.textContent = "Company: " + tenant.company
+                        tenantContactEmail.textContent = "Contact: " + tenant.contactEmail
+
+                        tenant.sites.forEach({ site ->
+                            val entry = document.createElement("tr") as HTMLTableRowElement
+                            val siteId = document.createElement("td") as HTMLTableCellElement
+                            siteId.textContent = site.id
+                            val siteSecret = document.createElement("td") as HTMLTableCellElement
+                            siteSecret.textContent = site.secret
+                            val siteName = document.createElement("td") as HTMLTableCellElement
+                            siteName.textContent = site.name
+                            entry.appendChild(siteName)
+                            entry.appendChild(siteId)
+                            entry.appendChild(siteSecret)
+                            document.getElementById("assignments")?.appendChild(entry)
+                        })
+                    })
+                }
+            }
+            xhr.send()
+        }
+    }
+    xhr.send()
+}
+
