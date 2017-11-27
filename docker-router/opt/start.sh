a69d0a79d36f1fb15099711659ee5eb5309463f6
Alex <alexander.orlov@loxal.net>
2017-11-28 00:06:46 +0100

    update Consul version

--- docker-router/opt/start.sh
+++ docker-router/opt/start.sh
@@ -2,4 +2,4 @@
 
 nginx -g 'daemon off;' &
 echo -n $LIVE > /var/live
-consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload"
+consul-template -consul-addr=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload"
--- docker-router/opt/switch.sh
+++ docker-router/opt/switch.sh
@@ -13,4 +13,4 @@ if [ $1 = "blue" ]
     echo -n "green" > /var/live
 fi
 
-consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload" -retry 30s -once
+consul-template -consul-addr=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload" -retry 30s -once
