6756ac50162485805fbf45bb3a3b087eb8608be9
Alex <alexander.orlov@loxal.net>
2017-11-28 09:57:07 +0100

    update SSL certificate script

---
+++ docker-router/update-ssl-certificate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# https://certbot.eff.org
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt update -y
+sudo apt install certbot -y
+
+docker stop router
+sudo certbot --standalone certonly --expand \
+    --domains "main.sitesearch.cloud, test.sitesearch.cloud" \
+    -d ci.sitesearch.cloud \
+    -d api.sitesearch.cloud \
+    -d dev.sitesearch.cloud \
+    -d www.sitesearch.cloud \
+    -d sitesearch.cloud \
+    -d doc.sitesearch.cloud \
+    -d logs.sitesearch.cloud \
+    -d blue.sitesearch.cloud \
+    -d green.sitesearch.cloud
+
+docker start router
