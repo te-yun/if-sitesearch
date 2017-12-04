245db2a474d75b9cc2f038f2fda3b18a0fb84b55
Alex <alexander.orlov@loxal.net>
2017-12-04 15:35:39 +0100

    adjust load tests

--- service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
+++ service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
@@ -82,7 +82,7 @@ public class LoadTest {
                 .warmupIterations(1)
                 .measurementIterations(20)
                 .forks(1)
-                .threads(50)
+                .threads(10)
                 .mode(Mode.Throughput)
                 .resultFormat(ResultFormatType.JSON)
                 .result("build/jmh-result.json")
