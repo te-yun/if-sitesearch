9f0aee0555a92145ee7db136b729c437d5cb72c1
Alex <alexander.orlov@loxal.net>
2017-12-09 11:43:53 +0100

    fix script

--- bootstrap/update-iFinder-core.sh
+++ bootstrap/update-iFinder-core.sh
@@ -3,5 +3,9 @@
 pwsh start-sitesearch-elasticsearch.ps1
 pwsh start-sitesearch-search-service.ps1
 
+ls ./opt/docker-compose-sitesearch.yaml
+ls opt/docker-compose-sitesearch.yaml
+ls ../opt/docker-compose-sitesearch.yaml
+
 docker-compose --file ./opt/docker-compose-sitesearch.yaml down
 docker-compose --file ./opt/docker-compose-sitesearch.yaml up -d
