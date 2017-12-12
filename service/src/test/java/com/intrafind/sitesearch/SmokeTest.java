13648d04e0d9197e37047d1941155b31f12d6f5b
Alex <alexander.orlov@loxal.net>
2017-12-12 15:48:09 +0100

    adjust smoke tests

--- service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
+++ service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
@@ -194,7 +194,7 @@ public class SmokeTest {
         assertNotNull(response.body());
         Hits result = MAPPER.readValue(response.body().bytes(), Hits.class);
         assertEquals(query, result.getQuery());
-        assertEquals(50, result.getResults().size());
+        assertTrue(40 < result.getResults().size());
         FoundPage found = result.getResults().get(0);
         assertEquals(268, found.getBody().length());
 
@@ -261,7 +261,7 @@ public class SmokeTest {
         final Response response = HTTP_CLIENT.newCall(request).execute();
         assertEquals(HttpStatus.OK.value(), response.code());
         Autocomplete result = MAPPER.readValue(response.body().bytes(), Autocomplete.class);
-        assertEquals(4, result.getResults().size());
+        assertTrue(5 < result.getResults().size());
         assureCorsHeaders(response.headers(), 406);
     }
 
