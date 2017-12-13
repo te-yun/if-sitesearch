0f0dcfc091aa62d24fae25dced9c0c32d262a163
Alex <alexander.orlov@loxal.net>
2017-12-13 18:22:20 +0100

    update API Health Status

--- bootstrap/start-elk.sh
+++ bootstrap/start-elk.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env pwsh
+#!/usr/bin/env sh
 
 ## Logstash
 #wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
@@ -11,5 +11,6 @@ docker network create blue-green
 docker network create ops
 sudo sysctl -w vm.max_map_count=262144
 
-docker-compose down; vim docker-compose.yaml; docker-compose up -d --force-recreate; docker-c
-ompose ps; docker ps;
+docker-compose down; vim docker-compose.yaml; docker-compose up -d --force-recreate; docker-compose ps; docker ps;
+
+curl -X PUT -u elastic http://<host>:9200/_xpack/license -H "Content-Type: application/json" -d @license-*.json
--- ci/update-searchbar.sh
+++ ci/update-searchbar.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+# TODO team
+
 curl http://nexus:8081/nexus/service/local/repositories/snapshots/content/com/intrafind/if-app-searchbar/5.1.3.2-SNAPSHOT/if-app-searchbar-5.1.3.2-20171212.090626-16.zip \
     -o data/
