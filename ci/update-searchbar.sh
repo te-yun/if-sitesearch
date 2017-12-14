2cbb19503f461a2861db0235eb1c6808b68fd542
Alex <alexander.orlov@loxal.net>
2017-12-14 12:29:22 +0100

    introduce searchbar update script

--- ci/update-searchbar.sh
+++ ci/update-searchbar.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
-# TODO team
+# TODO team productize
 
-curl http://nexus:8081/nexus/service/local/repositories/snapshots/content/com/intrafind/if-app-searchbar/5.1.3.2-SNAPSHOT/if-app-searchbar-5.1.3.2-20171212.090626-16.zip \
-    -o data/
+today=`date -u +"%Y-%m-%d"`
+cd service/src/main/resources/static/searchbar/build
+rm searchbar-latest.zip
+curl http://nexus:8081/nexus/service/local/repositories/snapshots/content/com/intrafind/if-app-searchbar/5.1.3.2-SNAPSHOT/if-app-searchbar-5.1.3.2-20171214.081958-18.zip \
+    -o searchbar-latest.zip
+
+rm -rf if-app-searchbar-*
+unzip searchbar-latest.zip
+mkdir ../$today
+mv if-app-searchbar-* ../$today/app
+cp -r ../latest/config ../$today
+cp -r ../latest/gadget ../$today
