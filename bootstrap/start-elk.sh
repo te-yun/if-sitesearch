8c087896d701ca249fe0ef2a0c1f2509f1916249
Alex <alexander.orlov@loxal.net>
2017-12-18 14:30:48 +0100

    update ELK start script

--- bootstrap/start-elk.sh
+++ bootstrap/start-elk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sudo sysctl -w vm.max_map_count=262144
-docker network create sitesearch
+#docker network create sitesearch
 
 docker-compose --file opt/docker-compose-elk.yaml down
 docker-compose --file opt/docker-compose-elk.yaml up -d --force-recreate
