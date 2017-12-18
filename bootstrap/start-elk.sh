3ec464aaa2f45a0961985d37a949b965f99648c9
Alex <alexander.orlov@loxal.net>
2017-12-18 16:44:42 +0100

    open UDP port for ELK

--- bootstrap/start-elk.sh
+++ bootstrap/start-elk.sh
@@ -9,5 +9,9 @@ docker-compose --file opt/docker-compose-elk.yaml -p tmp ps
 
 # Provide Basic License
 #docker exec -it ops-elasticsearch bash
-#vi license.json # paste license
+#vi license-sitesearch.json # paste license
 #curl -X PUT -u elastic http://localhost:9200/_xpack/license -H "Content-Type: application/json" -d @license-*.json
+
+# Delete index
+#curl localhost:9200/_cat/indices?v
+#curl -X DELETE localhost:9200/logstash-2017.11.20?pretty
