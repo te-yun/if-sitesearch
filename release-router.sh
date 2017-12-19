5e4f9fcdf3246779804fea466dd3ec2a40cd96f1
Alex <alexander.orlov@loxal.net>
2017-12-19 20:28:33 +0100

    update logstash address for dev

--- release-router.sh
+++ release-router.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env sh
 
-#function setupRedirectRouterContainer {
-    docker_network="sitesearch"
-    docker_redirect_image="router"
-    docker_tag="latest"
-    cd docker-${docker_redirect_image}
-    docker build --tag intrafind/${docker_redirect_image}:${docker_tag} .
-    docker rm -f $docker_redirect_image
-#    mkdir ~/srv/${docker_redirect_image}
-#        -e BLUE_NAME=blue `
-#        -e GREEN_NAME=green `
-#        -e LIVE=blue `
-#        -e CONSUL_URL=consul:8500 `
-#        -e constraint:node=master `
-    docker run -d --name $docker_redirect_image \
-        -p 80:80 \
-        -p 443:443 \
-        -v /etc/letsencrypt:/etc/letsencrypt \
-        --network $docker_network \
-        intrafind/${docker_redirect_image}:${docker_tag}
+docker_network=sitesearch
+docker_redirect_image=router
+docker_tag=latest
 
-    cd ..
-#}
-#setupRedirectRouterContainer
+cd docker-${docker_redirect_image}
+docker build --tag intrafind/${docker_redirect_image}:${docker_tag} .
+docker rm -f $docker_redirect_image
+docker run -d --name $docker_redirect_image \
+    -p 80:80 \
+    -p 443:443 \
+    -v /etc/letsencrypt:/etc/letsencrypt \
+    --network $docker_network \
+    intrafind/${docker_redirect_image}:${docker_tag}
+
+cd ..
--- release.sh
+++ release.sh
@@ -16,7 +16,7 @@ cd ..
 docker run -d --name ${docker_image_name} \
     -p 2443:8001 \
     --log-driver=gelf \
-    --log-opt gelf-address=udp://ops-logstash:12201 \
+    --log-opt gelf-address=udp://localhost:12201 \
     --env SECURITY_USER_PASSWORD=$SECURITY_USER_PASSWORD \
     --env BUILD_NUMBER=$BUILD_NUMBER \
     --env SCM_HASH=$SCM_HASH \
