6bd820ff115994e380a8ac9aecf95b6b772e23aa
Alex <alexander.orlov@loxal.net>
2017-12-20 20:41:35 +0100

    move throughput test to another site

--- service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
+++ service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
@@ -44,7 +44,7 @@ import static org.junit.Assert.*;
 @State(Scope.Benchmark)
 public class LoadTest {
     private static final Logger LOG = LoggerFactory.getLogger(LoadTest.class);
-    private static final UUID LOAD_SITE_ID = UUID.fromString("563714f1-96c0-4500-b366-4fc7e734fa1d");
+    private static final UUID LOAD_SITE_ID = UUID.fromString("8464f91e-df95-4e48-bfe0-f63af81e7346");
     static final String[] LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra enim vitae malesuada placerat. Nam auctor pellentesque libero, et venenatis enim molestie vel. Duis est metus, congue quis orci id, tincidunt mattis turpis. In fringilla ultricies sapien ultrices accumsan. Sed mattis tellus lacus, quis scelerisque turpis hendrerit et. In iaculis malesuada ipsum, ac rhoncus mauris auctor quis. Proin varius, ex vestibulum condimentum lacinia, ligula est finibus ligula, id consectetur nisi enim ut velit. Sed aliquet gravida justo ac condimentum. In malesuada sed elit vitae vestibulum. Mauris vitae congue lacus. Quisque vitae tincidunt orci. Donec viverra enim a lacinia pulvinar. Sed vel ullamcorper est. Vestibulum vel urna at nisl tincidunt blandit. Donec purus leo, interdum in diam in, posuere varius tellus. Quisque eleifend nulla at nulla vestibulum ullamcorper. Praesent interdum vehicula cursus. Morbi vitae nunc et urna rhoncus semper aliquam nec velit. Quisque aliquet et velit ut mollis. Sed mattis eleifend tristique. Praesent pharetra, eros eget viverra tempus, nisi turpis molestie metus, nec tristique nulla dolor a mauris. Nullam cursus finibus erat, in pretium urna fermentum ac. In hac habitasse platea dictumst. Cras id velit id nisi euismod eleifend. Duis vehicula gravida bibendum. Cras rhoncus, massa et accumsan euismod, metus arcu rutrum orci, eu porttitor lacus tellus sed quam. Morbi tincidunt est sit amet sem convallis porta in nec nisi. Sed ex enim, fringilla nec diam in, luctus pulvinar enim. Suspendisse potenti. Quisque ut pellentesque erat. In tincidunt metus id sem fringilla sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin erat nunc, pharetra sit amet iaculis nec, malesuada eu dui. Nullam sagittis ut arcu vitae convallis. Mauris molestie gravida lectus, eu commodo quam bibendum aliquam. Donec laoreet sed dolor eu consectetur."
             .split("\\s");
 
@@ -63,7 +63,7 @@ public class LoadTest {
     static {
         SEARCH_QUERIES.put("bank", 50L);
         SEARCH_QUERIES.put("fonds", 50L);
-        SEARCH_QUERIES.put("finanzen", 24L);
+        SEARCH_QUERIES.put("finanzen", 31L);
         SEARCH_QUERIES.put("geld", 50L);
         SEARCH_QUERIES.put("\uD83E\uDD84", 0L);
     }
