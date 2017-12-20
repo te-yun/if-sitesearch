5c510971cc7b38ed2fd8671aff18b1c7c8af4392
Alex <alexander.orlov@loxal.net>
2017-12-20 22:38:56 +0100

    update throughput tests

--- service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
+++ service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
@@ -94,10 +94,10 @@ public class LoadTest {
 
     static {
         AUTOCOMPLETE_QUERIES.put("hyp", 0L);
-        AUTOCOMPLETE_QUERIES.put("gel", 10L);
-        AUTOCOMPLETE_QUERIES.put("geld", 9L);
+        AUTOCOMPLETE_QUERIES.put("gel", 6L);
+        AUTOCOMPLETE_QUERIES.put("geld", 6L);
         AUTOCOMPLETE_QUERIES.put("ban", 10L);
-        AUTOCOMPLETE_QUERIES.put("bank", 9L);
+        AUTOCOMPLETE_QUERIES.put("bank", 6L);
         AUTOCOMPLETE_QUERIES.put("fond", 2L);
         QUERY_LIST_AUTOCOMPLETE = new ArrayList<>(AUTOCOMPLETE_QUERIES.keySet());
     }
