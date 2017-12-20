d881d0eedc5822a1357bf9fa5c9f9a5df8836cc9
Alex <alexander.orlov@loxal.net>
2017-12-20 11:18:57 +0100

    add project modifier to docker-compose

--- bootstrap/update-iFinder-core.sh
+++ bootstrap/update-iFinder-core.sh
@@ -3,5 +3,5 @@
 sh ./bootstrap/start-sitesearch-elasticsearch.sh
 sh ./bootstrap/start-sitesearch-search-service.sh
 
-docker-compose --file ./opt/docker-compose-sitesearch.yaml down
-docker-compose --file ./opt/docker-compose-sitesearch.yaml up -d
+docker-compose --file ./opt/docker-compose-sitesearch.yaml -p tmp down
+docker-compose --file ./opt/docker-compose-sitesearch.yaml -p tmp up -d
