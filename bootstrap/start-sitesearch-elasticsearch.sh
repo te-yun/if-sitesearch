18e92ba2692c0c404fa7ea398e95e062eafab58b
Alex <alexander.orlov@loxal.net>
2018-01-15 02:01:18 +0100

    cleanup update script

--- bootstrap/start-sitesearch-elasticsearch.sh
+++ bootstrap/start-sitesearch-elasticsearch.sh
@@ -6,22 +6,12 @@ docker_image="intrafind/$service_name"
 persistence_dir=srv
 HOME=/home/alexander_orlov
 
-#docker network create $docker_network
-
 if [ -f "$HOME/tmp/$service_name.tar" ]; then
-#if(Test-Path $HOME/tmp/$service_name.tar){
     docker load -i "$HOME/tmp/$service_name.tar"
 
-    mkdir -p $HOME/$persistence_dir/$service_name
-    sudo chown -R 1000:1000 $HOME/$persistence_dir/$service_name
-    sudo chmod -R 744 $HOME/$persistence_dir/$service_name
-
-#    docker rm -f $service_name
-#    docker run -d --name $service_name `
-#        --network $docker_network `
-#        -v $HOME/$persistence_dir/$service_name/data:/home/app_user/data:rw `
-#        -v $HOME/$persistence_dir/$service_name/logs:/home/app_user/logs:rw `
-#        $docker_image
+    mkdir -p ${HOME}/$persistence_dir/$service_name
+    sudo chown -R 1000:1000 ${HOME}/$persistence_dir/$service_name
+    sudo chmod -R 744 ${HOME}/$persistence_dir/$service_name
 else
     echo $HOME/tmp/$service_name.tar file is missing
 fi
