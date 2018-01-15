467c8daa4633966b400f9d38b3e75e25e79144eb
Alex <alexander.orlov@loxal.net>
2018-01-15 02:07:12 +0100

    cleanup update script

--- bootstrap/update-iFinder-core.sh
+++ bootstrap/update-iFinder-core.sh
@@ -3,7 +3,6 @@
 sh ./bootstrap/start-sitesearch-elasticsearch.sh
 sh ./bootstrap/start-sitesearch-search-service.sh
 
-echo $HOME---------------
-HOME=/home/alexander_orlov
-docker-compose --file ./opt/docker-compose-sitesearch.yaml -p tmp down
-docker-compose --file ./opt/docker-compose-sitesearch.yaml -p tmp up -d
+export HOME=/home/alexander_orlov
+docker-compose --file ./opt/docker-compose-sitesearch.yaml -p sitesearch down
+docker-compose --file ./opt/docker-compose-sitesearch.yaml -p sitesearch up -d
