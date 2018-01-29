f67f42cbec446537b4e2474cbb6a1d02677a02d3
Alex <alexander.orlov@loxal.net>
2018-01-29 12:02:59 +0100

    adjust tests

--- service/src/test/java/com/intrafind/sitesearch/integration/CrawlerTest.java
+++ service/src/test/java/com/intrafind/sitesearch/integration/CrawlerTest.java
@@ -16,9 +16,11 @@
 
 package com.intrafind.sitesearch.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.intrafind.sitesearch.controller.SitesController;
 import com.intrafind.sitesearch.dto.CrawlerJobResult;
+import okhttp3.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -27,7 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URLEncoder;
@@ -42,28 +43,46 @@ public class CrawlerTest {
     private static final UUID CRAWL_SITE_ID = UUID.fromString("a2e8d60b-0696-47ea-bc48-982598ee35bd");
     private static final UUID CRAWL_SITE_SECRET = UUID.fromString("04a0afc6-d89a-45c9-8ba8-41d393d8d2f8");
     private static final Logger LOG = LoggerFactory.getLogger(CrawlerTest.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final OkHttpClient HTTP_CLIENT = new OkHttpClient.Builder()
+            .followRedirects(false)
+            .followSslRedirects(false)
+            .build();
     @Autowired
     private TestRestTemplate caller;
 
 
     @Test
     public void crawlHttps() throws Exception {
-        final ResponseEntity<CrawlerJobResult> searchResults = caller.getForEntity(SitesController.ENDPOINT + "/" + CRAWL_SITE_ID + "/crawl?siteSecret=" + CRAWL_SITE_SECRET + "&url=" + URLEncoder.encode("https://api.sitesearch.cloud", Charsets.UTF_8.toString()), CrawlerJobResult.class);
+//        final ResponseEntity<CrawlerJobResult> searchResults = caller.getForEntity(SitesController.ENDPOINT + "/" + CRAWL_SITE_ID + "/crawl?siteSecret=" + CRAWL_SITE_SECRET + "&url=" + URLEncoder.encode("https://api.sitesearch.cloud", Charsets.UTF_8.toString()), CrawlerJobResult.class);
+        Request request = new Request.Builder()
+                .url(SitesController.ENDPOINT + "/" + CRAWL_SITE_ID + "/crawl?siteSecret=" + CRAWL_SITE_SECRET + "&url=" + URLEncoder.encode("https://api.sitesearch.cloud", Charsets.UTF_8.toString()))
+                .put(RequestBody.create(MediaType.parse("application/json"), ""))
+                .build();
+        final Response response = HTTP_CLIENT.newCall(request).execute();
+        CrawlerJobResult result = MAPPER.readValue(response.body().bytes(), CrawlerJobResult.class);
 
-        assertEquals(HttpStatus.OK, searchResults.getStatusCode());
-        assertNotNull(searchResults.getBody());
-        assertEquals(7, searchResults.getBody().getPageCount());
-        assertEquals(7, searchResults.getBody().getUrls().size());
+
+        assertEquals(HttpStatus.OK.value(), response.code());
+        assertNotNull(result);
+        assertEquals(7, result.getPageCount());
+        assertEquals(7, result.getUrls().size());
     }
 
     @Test
     public void crawlHttp() throws Exception {
-        final ResponseEntity<CrawlerJobResult> searchResults = caller.getForEntity(SitesController.ENDPOINT + "/" + CRAWL_SITE_ID + "/crawl?siteSecret=" + CRAWL_SITE_SECRET + "&url=" + URLEncoder.encode("http://example.com", Charsets.UTF_8.toString()), CrawlerJobResult.class);
+        Request request = new Request.Builder()
+                .url(SitesController.ENDPOINT + "/" + CRAWL_SITE_ID + "/crawl?siteSecret=" + CRAWL_SITE_SECRET + "&url=" + URLEncoder.encode("http://example.com", Charsets.UTF_8.toString()))
+                .put(RequestBody.create(MediaType.parse("application/json"), ""))
+                .build();
 
-        assertEquals(HttpStatus.OK, searchResults.getStatusCode());
-        assertNotNull(searchResults.getBody());
-        assertEquals(7, searchResults.getBody().getPageCount());
-        assertEquals(7, searchResults.getBody().getUrls().size());
+        final Response response = HTTP_CLIENT.newCall(request).execute();
+        CrawlerJobResult result = MAPPER.readValue(response.body().bytes(), CrawlerJobResult.class);
+
+        assertEquals(HttpStatus.OK.value(), response.code());
+        assertNotNull(result);
+        assertEquals(7, result.getPageCount());
+        assertEquals(7, result.getUrls().size());
     }
 }
 
