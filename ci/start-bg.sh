da80493a137ab119508bf2c954babcdb64d82404
Alex <alexander.orlov@loxal.net>
2018-01-30 23:42:26 +0100

    update consul

--- ci/start-bg.sh
+++ ci/start-bg.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-#docker_network="sitesearch"
-#docker network create $docker_network
-
 docker-compose --file opt/docker-compose-bg.yaml -p tmp down
 docker-compose --file opt/docker-compose-bg.yaml -p tmp up -d --force-recreate
 docker-compose --file opt/docker-compose-bg.yaml -p tmp ps
