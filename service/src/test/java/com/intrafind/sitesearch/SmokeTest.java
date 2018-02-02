1aed24538557bd22798b7fa9d062342ae615f8b7
Alex <alexander.orlov@loxal.net>
2018-02-02 16:11:08 +0100

    adjust smoke tests

--- service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
+++ service/src/test/java/com/intrafind/sitesearch/SmokeTest.java
@@ -154,9 +154,7 @@ public class SmokeTest {
         assertEquals(HttpStatus.OK.value(), response.code());
         assertNull(response.headers().get("x-frame-options"));
         assertNull(response.headers().get("X-Frame-Options"));
-//        response.headers().names().forEach(headerName -> LOG.info(headerName + ":" + response.headers().get(headerName)));
-//        assertEquals("gzip", response.headers().get("content-encoding"));
-        assureCorsHeaders(response.headers(), 456);
+        assureCorsHeaders(response.headers(), 449);
     }
 
     @Test
