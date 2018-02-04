b233fc470fc99aa7b331902e6aea8d01ed8f01de
Alex <alexander.orlov@loxal.net>
2018-02-04 13:30:14 +0100

    adjust tests

--- service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
+++ service/src/test/java/com/intrafind/sitesearch/jmh/LoadTest.java
@@ -75,7 +75,7 @@ public class LoadTest {
     static {
         AUTOCOMPLETE_QUERIES.put("hyp", 0L);
         AUTOCOMPLETE_QUERIES.put("geld", 6L);
-        AUTOCOMPLETE_QUERIES.put("bank", 9L);
+        AUTOCOMPLETE_QUERIES.put("bank", 7L);
         AUTOCOMPLETE_QUERIES.put("fond", 10L);
         QUERY_LIST_AUTOCOMPLETE = new ArrayList<>(AUTOCOMPLETE_QUERIES.keySet());
     }
