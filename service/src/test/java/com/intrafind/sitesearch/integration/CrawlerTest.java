e458198908711585b2077e881e49258784c0325b
Alex <alexander.orlov@loxal.net>
2018-02-05 19:20:36 +0100

    adjust number of pages for tests

--- service/src/test/java/com/intrafind/sitesearch/integration/CrawlerTest.java
+++ service/src/test/java/com/intrafind/sitesearch/integration/CrawlerTest.java
@@ -77,8 +77,8 @@ public class CrawlerTest {
 
         assertEquals(HttpStatus.OK, request.getStatusCode());
         assertNotNull(request.getBody());
-        assertEquals(13, request.getBody().getPageCount());
-        assertEquals(14, request.getBody().getUrls().size());
+        assertEquals(12, request.getBody().getPageCount());
+        assertEquals(13, request.getBody().getUrls().size());
     }
 
     @Test
