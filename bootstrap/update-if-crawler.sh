b23884e89cabc7ae2ab8e10db3cc7f7d35cdd5d1
Nopx <jollans94@googlemail.com>
2018-02-27 15:13:42 +0000

    Added debug statements and refactored

--- bootstrap/update-if-crawler.sh
+++ bootstrap/update-if-crawler.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env sh
 
-service_name=if-app-webcrawler
-docker_image="intrafind/$service_name"
-port_internal=8090
-port_external=$port_internal
 
-if [ $(docker ps -a -f="name=$service_name" -q | wc -c) -gt 0 ]
+SERVICE_NAME=if-app-webcrawler
+DOCKER_IMAGE="intrafind/$SERVICE_NAME"
+PORT_INTERNAL=8090
+PORT_EXTERNAL=$PORT_INTERNAL
+
+echo "Updating $SERVICE_NAME:"
+
+if [ $(docker ps -a -f="name=$SERVICE_NAME" -q | wc -c) -gt 0 ]
 then
-	docker stop $service_name
-	docker rm -v $service_name
+	echo "*	Container already exists"
+	echo "*	Killing container"
+	docker rm -f $SERVICE_NAME > nul
 fi
 
-docker run -p $port_external:$port_internal -d --name $service_name docker-registry.sitesearch.cloud/$docker_image
+echo "*	Creating new container $SERVICE_NAME"
+
+docker run -p $PORT_EXTERNAL:$PORT_INTERNAL -d --name $SERVICE_NAME docker-registry.sitesearch.cloud/$DOCKER_IMAGE > nul
