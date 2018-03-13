05be2479198d0226feba5fad76fbb60956d6bc1f
Alex <alexander.orlov@loxal.net>
2018-03-13 11:34:20 +0100

    finalize legal gadget

--- legal/src/main/kotlin/com/intrafind/legal/gadget/main.kt
+++ legal/src/main/kotlin/com/intrafind/legal/gadget/main.kt
@@ -16,48 +16,84 @@
 
 package com.intrafind.legal.gadget
 
-import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLDListElement
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLTextAreaElement
-import org.w3c.dom.events.Event
+import org.w3c.dom.events.CompositionEvent
+import org.w3c.dom.events.CompositionEventInit
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 import kotlin.browser.window
 
-fun main(args: Array<String>) {
+private fun main(args: Array<String>) {
+    window.addEventListener("DOMContentLoaded", {
+        init()
+    })
+
+    document.addEventListener(Legal.contractAnalyzeEvent, {
+        val event = it as CompositionEvent
+        console.warn(event.data)
+    })
 }
 
-private var siteId: String = ""
-private var siteSecret: String = ""
-private var websiteUrl: String = ""
 private val serviceUrl: String = if (window.location.hostname.equals("localhost")) {
     "http://localhost:8001"
 } else {
     "https://api.sitesearch.cloud"
 }
 
-fun showInitCode() {
-
-
+private lateinit var analyzedContractContainer: HTMLDListElement
+private lateinit var contractContainer: HTMLTextAreaElement
+private fun init() {
+    analyzedContractContainer = document.getElementById("analyzedContract") as HTMLDListElement
+    contractContainer = document.getElementById("contract") as HTMLTextAreaElement
 }
 
 fun analyze() {
     val xhr = XMLHttpRequest()
     xhr.open("PUT", "$serviceUrl/legal/37abd346-261b-11e8-989a-63440ea45232/contract/gist")
+    xhr.send(contractContainer.value)
     xhr.onload = {
-        console.warn(xhr.responseText)
-        console.warn(JSON.parse<dynamic>(xhr.responseText).tags)
-        val s = JSON.stringify(JSON.parse<dynamic>(xhr.responseText).tags)
-        (document.getElementById("result") as HTMLDivElement).textContent = s
-        document.dispatchEvent(Event("sis.contract.analyze"))
+        val analyzedContract: AnalyzedContract = JSON.parse(xhr.responseText)
+        displayContractGist(analyzedContract)
+        document.dispatchEvent(CompositionEvent(Legal.contractAnalyzeEvent, CompositionEventInit(xhr.responseText)))
     }
-    xhr.setRequestHeader("content-type", "application/json")
-    xhr.send((document.getElementById("contract") as HTMLTextAreaElement).value)
+}
+
+private fun displayContractGist(analyzedContract: dynamic) {
+    fun showTypeListings(typeName: String) {
+        val type = document.createElement("dt") as HTMLElement
+        analyzedContractContainer.appendChild(type)
+        for (tag in analyzedContract.tags) {
+            if (tag.type.equals(typeName)) {
+                type.textContent = tag.type
+
+                val mainForm = document.createElement("dd") as HTMLElement
+                mainForm.textContent = tag.mainForm
+                analyzedContractContainer.appendChild(mainForm)
+
+                val otherForms = document.createElement("dd") as HTMLElement
+                otherForms.textContent = tag.otherForms
+                analyzedContractContainer.appendChild(otherForms)
+
+                val displayform = document.createElement("dd") as HTMLElement
+                displayform.textContent = tag.features.displayform
+                analyzedContractContainer.appendChild(displayform)
+            }
+        }
+    }
+    showTypeListings("Location")
+    showTypeListings("Organization")
+    showTypeListings("Person")
+    showTypeListings("PersonRole")
 }
 
 class Legal {
     companion object {
-        val captchaSiteKey = ""
+        val contractAnalyzeEvent = "sis.contract.analyze"
     }
 }
 
-data class SiteProfileCreation(val urls: Set<String>, val email: String)
+data class AnalyzedContract(val tags: List<Tag>)
+data class Tag(val mainForm: String, val type: String, val otherForms: List<String>, val features: Features)
+data class Features(val displayform: String)
