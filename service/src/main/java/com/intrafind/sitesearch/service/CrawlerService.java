7e45382afbb4abf49d93f9908a378690befab31c
Alex <alexander.orlov@loxal.net>
2018-03-15 10:51:46 +0100

    make throttled crawler multi-threaded

--- service/src/main/java/com/intrafind/sitesearch/service/CrawlerService.java
+++ service/src/main/java/com/intrafind/sitesearch/service/CrawlerService.java
@@ -43,7 +43,7 @@ public class CrawlerService {
         config.setCrawlStorageFolder(CRAWLER_STORAGE);
         final int crawlerThreads;
         if (isThrottled) {
-            crawlerThreads = 1;
+            crawlerThreads = 2;
             config.setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
             config.setPolitenessDelay(200);
             config.setMaxOutgoingLinksToFollow(1_000);
