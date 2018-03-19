2aedd4d6e0044722ad5ff24963ba3441f5344c0e
Nopx <jollans94@googlemail.com>
2018-03-19 15:29:24 +0100

    Adapted search service start to the existence of a registry

--- bootstrap/start-sitesearch-search-service.sh
+++ bootstrap/start-sitesearch-search-service.sh
@@ -5,22 +5,26 @@ service_name=sitesearch-search-service
 docker_image="intrafind/$service_name"
 persistence_dir=srv
 HOME=/home/alexander_orlov
+docker_registry="docker-registry.sitesearch.cloud12"
 
-#docker network create $docker_network
+docker network create $docker_network
 
-if [ -f "$HOME/tmp/$service_name.tar" ]; then
-#if(Test-Path $HOME/tmp/$service_name.tar){
-    docker load -i "$HOME/tmp/$service_name.tar"
-
-#    mkdir -p $HOME/$persistence_dir/$service_name
-#    sudo chown -R 1000:1000 $HOME/$persistence_dir/$service_name
-#    sudo chmod -R 744 $HOME/$persistence_dir/$service_name
-#
-#    docker rm -f $service_name
-#    docker run -d --name $service_name --network $docker_network `
-#        -p 9605:9605 `
-#        -v $HOME/$persistence_dir/$service_name/logs:/home/app_user/logs `
-#        $docker_image
-else
-    echo $HOME/tmp/$service_name.tar file is missing
+#the ~ is not a problem, since the home directory is where docker looks for credentials anyway
+if ! grep -q $docker_registry ~/.docker/config.json ;then
+  echo "Not logged in to registry"
+  #TODO Handle case (log in with environment variable?)
+	#docker login $docker_registry
 fi
+
+docker run -d $docker_registry/$docker_image
+
+
+# ******************************************************
+# ** Deprecated Code (delete after next edit of file) **
+# ******************************************************
+#if [ -f "$HOME/tmp/$service_name.tar" ]; then
+#if(Test-Path $HOME/tmp/$service_name.tar){
+#    docker load -i "$HOME/tmp/$service_name.tar"
+#else
+#    echo $HOME/tmp/$service_name.tar file is missing
+#fi
