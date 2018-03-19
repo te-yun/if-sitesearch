b695516b135f45611d739b5474ad98140c5324a0
Nopx <jollans94@googlemail.com>
2018-03-19 15:47:01 +0100

    Removed error checks and deprecated code

--- bootstrap/start-sitesearch-search-service.sh
+++ bootstrap/start-sitesearch-search-service.sh
@@ -9,22 +9,4 @@ docker_registry="docker-registry.sitesearch.cloud12"
 
 docker network create $docker_network
 
-#the ~ is not a problem, since the home directory is where docker looks for credentials anyway
-if ! grep -q $docker_registry ~/.docker/config.json ;then
-  echo "Not logged in to registry"
-  #TODO Handle case (log in with environment variable?)
-	#docker login $docker_registry
-fi
-
 docker run -d $docker_registry/$docker_image
-
-
-# ******************************************************
-# ** Deprecated Code (delete after next edit of file) **
-# ******************************************************
-#if [ -f "$HOME/tmp/$service_name.tar" ]; then
-#if(Test-Path $HOME/tmp/$service_name.tar){
-#    docker load -i "$HOME/tmp/$service_name.tar"
-#else
-#    echo $HOME/tmp/$service_name.tar file is missing
-#fi
