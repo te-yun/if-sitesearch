1f84ff6a68438b21fea041f13d18c5ae641e46e7
Alex <alexander.orlov@loxal.net>
2018-03-20 16:23:31 +0100

    update iFinder core upgrade mechanism (untested)

--- bootstrap/start-sitesearch-elasticsearch.sh
+++ bootstrap/start-sitesearch-elasticsearch.sh
@@ -6,12 +6,6 @@ docker_image="intrafind/$service_name"
 persistence_dir=srv
 HOME=/home/alexander_orlov
 
-if [ -f "$HOME/tmp/$service_name.tar" ]; then
-    docker load -i "$HOME/tmp/$service_name.tar"
-
     mkdir -p ${HOME}/$persistence_dir/$service_name
     sudo chown -R 1000:1000 ${HOME}/$persistence_dir/$service_name
     sudo chmod -R 744 ${HOME}/$persistence_dir/$service_name
-else
-    echo $HOME/tmp/$service_name.tar file is missing
-fi
--- bootstrap/start-sitesearch-search-service.sh
+++ bootstrap/start-sitesearch-search-service.sh
@@ -6,21 +6,13 @@ docker_image="intrafind/$service_name"
 persistence_dir=srv
 HOME=/home/alexander_orlov
 
-#docker network create $docker_network
+docker network create $docker_network
 
-if [ -f "$HOME/tmp/$service_name.tar" ]; then
-#if(Test-Path $HOME/tmp/$service_name.tar){
-    docker load -i "$HOME/tmp/$service_name.tar"
+prepare_persistence_directory() {
+    mkdir -p $HOME/$persistence_dir/$1/data
+    sudo chown -R 1000:1000 $HOME/$persistence_dir/$1/data
+    sudo chmod -R 744 $HOME/$persistence_dir/$1/data
+}
 
-#    mkdir -p $HOME/$persistence_dir/$service_name
-#    sudo chown -R 1000:1000 $HOME/$persistence_dir/$service_name
-#    sudo chmod -R 744 $HOME/$persistence_dir/$service_name
-#
-#    docker rm -f $service_name
-#    docker run -d --name $service_name --network $docker_network `
-#        -p 9605:9605 `
-#        -v $HOME/$persistence_dir/$service_name/logs:/home/app_user/logs `
-#        $docker_image
-else
-    echo $HOME/tmp/$service_name.tar file is missing
-fi
+prepare_persistence_directory sitesearch-search-service
+prepare_persistence_directory sitesearch-search-service-1
