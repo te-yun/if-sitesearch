940f947de4444df52fa05de94662d7505323483a
Alex <alexander.orlov@loxal.net>
2018-03-29 17:22:14 +0200

    cleanup

--- gadget/src/main/kotlin/com/intrafind/sitesearch/gadget/main.kt
+++ gadget/src/main/kotlin/com/intrafind/sitesearch/gadget/main.kt
@@ -24,10 +24,8 @@ import org.w3c.dom.events.Event
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 import kotlin.browser.window
-import kotlin.coroutines.experimental.suspendCoroutine
 import kotlin.dom.addClass
 import kotlin.dom.removeClass
-import kotlin.js.Promise
 
 suspend fun main(args: Array<String>) {
     window.addEventListener("DOMContentLoaded", {
@@ -37,8 +35,6 @@ suspend fun main(args: Array<String>) {
                 "    });"
         )
     })
-
-//    tests()
 }
 
 private var siteId: String = ""
@@ -233,59 +229,5 @@ class SiteSearch {
         val captchaSiteKey = "6LflVEQUAAAAANVEkwc63uQX96feH1H_6jDU-Bn5"
     }
 }
-
-//suspend fun httpGet(url: String): String = suspendCoroutine { c ->
-//    val xhr = XMLHttpRequest()
-//    xhr.onreadystatechange = {
-//        if (xhr.readyState == XMLHttpRequest.DONE) {
-//            if (xhr.status / 100 == 2) {
-//                c.resume(xhr.response as String)
-//            }
-//            else {
-//                c.resumeWithException(RuntimeException("HTTP error: ${xhr.status}"))
-//            }
-//        }
-//        null
-//    }
-//    xhr.open("GET", url)
-//    xhr.send()
-//
-//}
-
-suspend fun tests() {
-    openFile("https://yesno.wtf/api").await()
-}
-
-suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
-    then({ cont.resume(it) }, { cont.resumeWithException(it) })
-}
-
-fun openFile(url: String): Promise<XMLHttpRequest> {
-    val p = Promise<XMLHttpRequest> { resolve, reject ->
-        val xhr = XMLHttpRequest()
-        xhr.open("GET", url)
-        xhr.addEventListener("load", { e ->
-            resolve(xhr)
-        })
-        xhr.send()
-    }
-    return p
-}
-
-//fun <T> async(x: suspend () -> T): Promise<T> {
-//    return Promise { resolve, reject ->
-//        x.startCoroutine(object : Continuation<T> {
-//            override val context = EmptyCoroutineContext
-//
-//            override fun resume(value: T) {
-//                resolve(value)
-//            }
-//
-//            override fun resumeWithException(exception: Throwable) {
-//                reject(exception)
-//            }
-//        })
-//    }
-//}
 
 data class SiteProfileCreation(val urls: Set<String>, val email: String)
