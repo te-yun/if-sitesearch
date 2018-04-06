b45e2c825e18440db73d9fe622164163783e5ec5
Battsengel Ichinnorov <ichinnorovb@gmail.com>
2018-04-06 12:03:37 +0200

    set local environment variable (#6)
    set local environment variables

--- set-local-dev-dummy-env.sh
+++ set-local-dev-dummy-env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-<<<<<<< HEAD
 # Execute this to set all required environment variables, in order to launch service.
 
 export ADMIN_SITE_SECRET=82ba4106-397b-11e8-9155-0a0027000009
@@ -8,13 +7,3 @@ export RECAPTCHA_SITE_SECRET=82ba4106-397b-11e8-9155-0a0027000009
 export SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=82ba4106-397b-11e8-9155-0a0027000009
 export SPRING_SECURITY_USER_PASSWORD=82ba4106-397b-11e8-9155-0a0027000009
 export DEV_SKIP_FLAG=true
-=======
-# To build and run site-search properly you need those dummy environment variables below
-# use one of the created uuid from uuid in exports commands below
-
-export ADMIN_SITE_SECRET=""
-export RECAPTCHA_SITE_SECRET=""
-export SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=""
-export SPRING_SECURITY_USER_PASSWORD=""
-export DEV_SKIP_FLAG="true"
->>>>>>> 7d68c69227cb4de779433f5f1b002881479a1e4b
