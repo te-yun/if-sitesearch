753a85ec42f897784193f1b115591ec7bdbd071e
Alex <alexander.orlov@loxal.net>
2018-04-10 17:01:24 +0200

    introduce dashboard gadget

---
+++ dashboard/src/main/kotlin/com/intrafind/sitesearch/dashboard/main.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intrafind.sitesearch.dashboard
+
+import org.w3c.dom.HTMLDivElement
+import kotlin.browser.document
+import kotlin.browser.window
+
+private suspend fun main(args: Array<String>) {
+    window.addEventListener("DOMContentLoaded", {
+        init()
+    })
+}
+
+private var siteId: String = ""
+private var siteSecret: String = ""
+private val serviceUrl: String = window.location.origin
+
+private lateinit var siteIdContainer: HTMLDivElement
+private lateinit var siteIdBox: HTMLDivElement
+private lateinit var siteSecretContainer: HTMLDivElement
+private lateinit var siteSecretBox: HTMLDivElement
+
+fun init() {
+    siteIdContainer = document.getElementById("siteId") as HTMLDivElement
+    siteSecretContainer = document.getElementById("siteSecret") as HTMLDivElement
+    siteIdBox = document.getElementById("siteIdContainer") as HTMLDivElement
+    siteSecretBox = document.getElementById("siteSecretContainer") as HTMLDivElement
+
+    applyQueryParameters()
+}
+
+fun applyQueryParameters() {
+    siteId = window.location.search.substring(window.location.search.indexOf("siteId=") + 7, 44)
+    siteSecret = window.location.search.substring(window.location.search.indexOf("siteSecret=") + 11, 44)
+
+    console.warn(siteId)
+    console.warn(siteSecret)
+}
+
+class SiteSearch {
+    companion object
+}
