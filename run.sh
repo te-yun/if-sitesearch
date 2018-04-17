1620810283ee130353f0035b93c8a7d87a8feb1b
Alex <alexander.orlov@loxal.net>
2018-04-17 10:28:30 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
 {
-    SPRING_CONFIG_NAME="application, local" ./gradlew bootRun \
-        --continue --continuous --no-scan --parallel --build-cache \
-        $1
+    SPRING_CONFIG_NAME="application, local" ./gradlew bootRun --continue --continuous --no-scan --parallel --build-cache $1
 } || {
     hangingJavaProcessToStop=`jps | grep Application | awk '{print $1}'`
     kill -9 $hangingJavaProcessToStop
