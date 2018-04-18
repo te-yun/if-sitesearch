4e6b8e327923b90c87b0966faad578297daad07f
Nopx <jollans94@googlemail.com>
2018-04-18 09:30:26 +0000

    overwrite old de_docker version with master version

--- bootstrap/start-sitesearch-search-service.sh
+++ bootstrap/start-sitesearch-search-service.sh
@@ -5,7 +5,6 @@ service_name=sitesearch-search-service
 docker_image="intrafind/$service_name"
 persistence_dir=srv
 HOME=/home/alexander_orlov
-docker_registry="docker-registry.sitesearch.cloud12"
 
 docker network create $docker_network
 
