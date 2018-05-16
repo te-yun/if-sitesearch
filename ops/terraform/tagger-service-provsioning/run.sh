028b2b018d2a96e086b5fe12893344176ec5317c
Nopx <jollans94@googlemail.com>
2018-05-16 08:12:10 +0000

    fixed paths in terraform tagger service script

--- ops/terraform/tagger-service-provsioning/run.sh
+++ ops/terraform/tagger-service-provsioning/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-
+cd ops/terraform/tagger-service-provsioning/
 terraform init
-terraform apply -var-file=existing-state.tfvars
+terraform apply -var-file=existing-state.tfvars -auto-approve
