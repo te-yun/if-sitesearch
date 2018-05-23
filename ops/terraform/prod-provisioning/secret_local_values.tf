28a4e18be59b333b5f7a34733d59ef97dc4f32ae
Nopx <jollans94@googlemail.com>
2018-05-23 12:23:57 +0200

    Fixed bug in secret local values

--- ops/terraform/prod-provisioning/secret_local_values.tf
+++ ops/terraform/prod-provisioning/secret_local_values.tf
@@ -2,7 +2,7 @@ locals {
   credentials = "${file("/srv/minion/compute-engine-default-service-account.json")}"
   ssh_user = "ubuntu"
   ssh_private_key = "${file("/srv/minion/.ssh/ubuntu-tf_key")}"
-  ssh_private_key = "/srv/minion/.ssh/ubuntu-tf_key"
+  ssh_private_key_path = "/srv/minion/.ssh/ubuntu-tf_key"
   gcloud_project = "woven-alpha-150909"
   docker-address = "docker-registry.sitesearch.cloud"
   docker-username = "sitesearch"
