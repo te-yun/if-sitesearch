a3fe8a1c4e95b71ede39a2018e9df8f7f9105b5e
Nopx <jollans94@googlemail.com>
2018-05-23 10:57:28 +0200

    Added tagger-service to prod-provisioning

--- ops/terraform/prod-provisioning/modules/setup/kubernetes_module/kubelet.tf
+++ ops/terraform/prod-provisioning/modules/setup/kubernetes_module/kubelet.tf
@@ -117,6 +117,32 @@ resource "null_resource" "Kubernetes-Metaobjects" {
 #	}
 #}
 
+#Tagging Service
+resource "null_resource" "Tagging-Service"{
+
+	depends_on=["module.master","null_resource.Kubernetes-Metaobjects"]
+
+	triggers = {
+		kubernetes_configuration_hash = "${sha1(file("./modules/setup/kubernetes_module/kubernetes_templates/kube_tagging-service-template.yaml"))}"
+	}
+
+	//turning the rendered template to a file and then executing
+	provisioner "remote-exec" {
+		inline=["export KUBECONFIG=$HOME/admin.conf ",
+						"cat <<EOT > kube_tagging-service.yaml",
+						"${data.template_file.kubernetes_tagging-service.rendered}",
+						"EOT",
+						"if [[ $(kubectl get pod if-tagger) ]]; then kubectl replace -f kube_tagging-service.yaml ; else kubectl apply -f kube_tagging-service.yaml; fi"]
+	}
+
+	connection {
+			user = "${var.ssh_user}"
+			//Access IP of newly provisioned machine
+			host = "${var.ip_address_list[0]}"
+			type = "ssh"
+			private_key="${var.ssh_private_key}"
+	}
+}
 
 #Elasticsearch
 resource "null_resource" "Sitesearch-Elasticsearch"{
