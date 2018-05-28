804c58726860fd77575d7fd50c8e25b5eb661435
Nopx <jollans94@googlemail.com>
2018-05-28 11:28:55 +0200

    adapted variables and variable names, to make it more readable and more easily changable

--- ops/terraform/prod-provisioning/main.tf
+++ ops/terraform/prod-provisioning/main.tf
@@ -8,11 +8,11 @@ terraform {
 }*/
 
 module "google" {
-	count="${var.total_vm_count - length(var.ip_pool)}"
-	source="./modules/provision/google_module"
+	count="${var.total_vm_count - length(var.kubernetes_existing_nodes_ip_pool)}"
+	source="./modules/vm_provisioning/google_module"
 	project = "${local.gcloud_project}"
 	credentials = "${local.credentials}"
-	name_machine = "google"
+	name_machine = "kubernetes-node"
 }
 
 module "kubernetes" {
@@ -20,7 +20,7 @@ module "kubernetes" {
 	source="./modules/setup/kubernetes_module"
 
 	count="${var.total_vm_count}"
-	ip_address_list="${concat(var.ip_pool,module.google.external_ip_address_list)}"
+	ip_address_list="${concat(var.kubernetes_existing_nodes_ip_pool,module.google.external_ip_address_list)}"
 	ssh_private_key = "${local.ssh_private_key}"
 	ssh_private_key_path = "${local.ssh_private_key_path}"
 	ssh_user = "${local.ssh_user}"
@@ -31,7 +31,7 @@ module "kubernetes" {
 			"secret_name" = "sitesearch-secret"
 	}
 
-	master_node_name = "dev"
+	master_node_name = "${length(var.kubernetes_existing_nodes_names) > 0? var.kubernetes_existing_nodes_names[0] : "kubernetes-node0"}"
 	google_project = "${local.gcloud_project}"
 
 	kube_counts = {
