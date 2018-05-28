00a920d69411862662afe17af2b09a6bc1b926cc
bachka <ichinnorovb@gmail.com>
2018-05-28 16:10:38 +0200

    improved bootstrap redeploy scripts for tagging-service and sis-core

--- bootstrap/redeploy-tagger-service.sh
+++ bootstrap/redeploy-tagger-service.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-cd /home/ubuntu/if-sitesearch/ops/terraform/prod-provisioning/
+cd /srv/if-sitesearch/ops/terraform/prod-provisioning/
 sudo -u ubuntu terraform taint -module=kubernetes null_resource.Tagging-Service
 sudo -u ubuntu terraform apply -auto-approve
--- bootstrap/redeploy-terraform-all.sh
+++ bootstrap/redeploy-terraform-all.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
-cd /home/ubuntu/if-sitesearch/ops/terraform/prod-provisioning/
+cd /srv/if-sitesearch/ops/terraform/prod-provisioning/
 sudo -u ubuntu terraform destroy -auto-approve
+# if you want have complete new state use the code below
+# rm -rf terraform.tfstate
+# rm -rf .terraform/
+# sudo -u ubuntu terraform init
+# ======================================================
 sudo -u ubuntu terraform apply -auto-approve
