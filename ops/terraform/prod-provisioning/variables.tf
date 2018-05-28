e9335b4d1eab98d8ba74c76be73b7d8266d7cc9c
Nopx <jollans94@googlemail.com>
2018-05-28 11:34:04 +0200

    adapted variables and variable names, to make it more readable and more easily changable

--- ops/terraform/prod-provisioning/variables.tf
+++ ops/terraform/prod-provisioning/variables.tf
@@ -1,5 +1,9 @@
-variable "ip_pool"{
+variable "kubernetes_existing_nodes_ip_pool"{
   description = "The pool of IPs that already exist."
+  type = "list"
+}
+variable "kubernetes_existing_nodes_names"{
+  description = "The names of the VMs that already exist."
   type = "list"
 }
 variable "total_vm_count"{
