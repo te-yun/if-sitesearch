8bfad8d44522fad5356a83d8e2931de6dfef3b8d
Alex <alexander.orlov@loxal.net>
2018-06-26 00:19:47 +0200

    introduce cleaned up github OAuth2 implementation

--- service/src/main/java/com/intrafind/sitesearch/WebSecurityConfigurer.java
+++ service/src/main/java/com/intrafind/sitesearch/WebSecurityConfigurer.java
@@ -16,23 +16,79 @@
 
 package com.intrafind.sitesearch;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 
+import javax.servlet.Filter;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableOAuth2Sso
 @EnableWebSecurity
 @RestController
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+    private final OAuth2ClientContext oauth2ClientContext;
+
     @RequestMapping("user")
     public Principal user(final Principal principal) {
         return principal;
+    }
+
+    @Autowired
+    public WebSecurityConfigurer(final OAuth2ClientContext oauth2ClientContext) {
+        this.oauth2ClientContext = oauth2ClientContext;
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(final OAuth2ClientContextFilter filter) {
+        final FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    @Bean
+    @ConfigurationProperties("github")
+    public ClientResources github() {
+        return new ClientResources();
+    }
+
+    private Filter ssoFilter() {
+        final CompositeFilter filter = new CompositeFilter();
+        final List<Filter> filters = new ArrayList<>();
+        filters.add(ssoFilter(github(), "/login/github"));
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private Filter ssoFilter(final ClientResources client, final String path) {
+        final OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+        final OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+        filter.setRestTemplate(template);
+        final UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
+        tokenServices.setRestTemplate(template);
+        filter.setTokenServices(tokenServices);
+        return filter;
     }
 
     @Override
@@ -47,6 +103,24 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 )
                 .permitAll()
                 .and().logout().logoutSuccessUrl("https://sitesearch.cloud")
+                .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
         ;
+    }
+
+    private class ClientResources {
+
+        @NestedConfigurationProperty
+        private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+        @NestedConfigurationProperty
+        private ResourceServerProperties resource = new ResourceServerProperties();
+
+        public AuthorizationCodeResourceDetails getClient() {
+            return client;
+        }
+
+        public ResourceServerProperties getResource() {
+            return resource;
+        }
     }
 }
