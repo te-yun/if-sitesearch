c573b30c6374ff21853f69c669a0bc6b9571afa3
Alex <alexander.orlov@loxal.net>
2018-06-29 00:20:38 +0200

    remove Legal Service

--- ops/recrawl.sh
+++ ops/recrawl.sh
@@ -18,8 +18,8 @@ curl -X GET \
     "https://api.sitesearch.cloud/sites/crawl/status?serviceSecret=${ADMIN_SITE_SECRET}" \
     -o $SITE_CRAWL_STATUS_FILE
 
-sudo apt install -y jq
-apt install -y jq
+sudo apt-get install -y jq
+apt-get install -y jq
 failedCrawlStatusList=$(cat $SITE_CRAWL_STATUS_FILE | jq -r '.sites[] | select (.pageCount | length  == 0 )');
 
 # if failedCrawlStatusList not empty give me the siteIDs
--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ docker_tag=latest
 docker_image_name=if-sitesearch
 img_fqn=docker-registry.sitesearch.cloud/intrafind/${docker_image_name}:${docker_tag}
 
-cp -r /home/alexander_orlov/docker-build-data/api-sitesearch/service .
+cp -r /home/ubuntu/docker-build-data/api-sitesearch/service .
 cd service
 docker build --tag intrafind/${docker_image_name}:${docker_tag} .
 docker rm -f ${docker_image_name}
@@ -31,8 +31,25 @@ docker run -d --name ${docker_image_name} \
     --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET \
     --network $docker_network \
     intrafind/${docker_image_name}:${docker_tag}
-
 docker push ${img_fqn}
+
+docker run -d --name sis-api \
+    --log-driver=gelf \
+    --log-opt gelf-address=udp://localhost:12201 \
+    --env SIS_API_SERVICE_URL=$SIS_API_SERVICE_URL \
+    --env SERVICE_SECRET=$SERVICE_SECRET \
+    --env SIS_SERVICE_HOST=$SIS_SERVICE_HOST \
+    --env WOO_COMMERCE_CONSUMER_KEY="$WOO_COMMERCE_CONSUMER_KEY" \
+    --env WOO_COMMERCE_CONSUMER_SECRET="$WOO_COMMERCE_CONSUMER_SECRET" \
+    --env BASIC_ENCODED_PASSWORD="$BASIC_ENCODED_PASSWORD" \
+    --env ADMIN_SITE_SECRET=$ADMIN_SITE_SECRET \
+    --env INVISIBLE_RECAPTCHA_SITE_SECRET=${INVISIBLE_RECAPTCHA_SITE_SECRET} \
+    --env SPRING_SECURITY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD \
+    --env BUILD_NUMBER=$BUILD_NUMBER \
+    --env SCM_HASH=$SCM_HASH \
+    --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET \
+    --network $docker_network \
+    intrafind/sis-api:latest
 
 danglingImages=$(docker images -f "dangling=true" -q)
 if [ "$danglingImages" ]; then
