231156d35c0b507c1fcb3b55211a13ea5426d8f4
Alex <alexander.orlov@loxal.net>
2018-07-11 13:54:11 +0200

    update WP plugin

--- service/src/main/resources/static/external/wordpress-plugin/sitesearch/sis-admin-page.php
+++ service/src/main/resources/static/external/wordpress-plugin/sitesearch/sis-admin-page.php
@@ -1,14 +1,23 @@
+<script>
+    var registerSiteInSiS = function () {
+        console.warn("TODO: Create siteId/siteSecret with a POST to api.sitesearch.cloud/sites" + document.getElementById("sis-url").value);
+        console.warn("TODO: Submit URL to api.sitesearch.cloud, using siteId/Secret from the previous call." + document.getElementById("sis-url").value);
+    };
+</script>
 <div><!-- wrapper div-->
-	<h1>Configure your Site Search:</h1>
-	<?php		 
-		$url = get_site_url();
-	?>
-	<form method="post"> 
-		 	Website URL: <input type="text" name="url" value="<?php echo $url;?>">
-			<br><br>
-			<input type="submit" name="submit" value="Start crawling your Site">
-	</form>
-	<?php
-		echo $url;		
-	?>
+    <h1>Configuration</h1>
+    <?php
+    $url = get_site_url();
+    ?>
+    <form method="post">
+        Website URL: <input type="text" id="sis-url" name="url" value="<?php echo $url; ?>">
+        <br><br>
+        <input type="submit"
+               name="submit" value="Add Site Search searchbar to your site &amp; crawl your site's content."
+               onclick="registerSiteInSiS();"
+        >
+    </form>
+    <?php
+    echo $url;
+    ?>
 </div><!-- wrapper div-->
