2627cb66142c4a064520774bf469f6c474f84d51
Alex <alexander.orlov@loxal.net>
2018-07-16 15:43:06 +0200

    introduce Java 11 module

--- service/src/test/java/com/intrafind/sitesearch/SimpleClientTest.java
+++ service/src/test/java/com/intrafind/sitesearch/SimpleClientTest.java
@@ -16,10 +16,22 @@
 
 package com.intrafind.sitesearch;
 
-import com.intrafind.api.Document;
+import com.intrafind.sitesearch.dto.Subscription;
 import com.intrafind.sitesearch.service.SimpleAutocompleteClient;
 import com.intrafind.sitesearch.service.SimpleIndexClient;
 import com.intrafind.sitesearch.service.SimpleSearchClient;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -29,6 +41,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -44,25 +62,56 @@ public class SimpleClientTest {
         final var simpleAutocomplete = new SimpleAutocompleteClient();
         final var simpleSearch = new SimpleSearchClient();
 
-        var doc = new Document("sis-test");
-        doc.set("key", "value");
-        simpleIndex.index(doc);
-        assertTrue(true);
-        assertTrue(true);
+        IndexRequest indexRequest = new IndexRequest(
+                "sis",
+                "doc",
+                "4");
+        final var jsonString = "{" +
+                "\"java11\":\"KIMCHY\"," +
+                "\"post\":\"2013-01-30\"," +
+                "\"message\":\"TRYING 12 ELASTICSEARCH\"" +
+                "}";
+        indexRequest.source(jsonString, XContentType.JSON);
 
-
-//        final CreateIndexRequest request = new CreateIndexRequest("sis-config");
-//        request.settings(Settings.builder()
+        final CreateIndexRequest createIndexRequest = new CreateIndexRequest("sis");
+//        createIndexRequest.settings(Settings.builder()
 //                .put("index.number_of_shards", 3)
 //                .put("index.number_of_replicas", 2)
 //        );
-//
-//
-//        final RestHighLevelClient client = new RestHighLevelClient(
-//                RestClient.builder(
-//                        new HttpHost("localhost", 9200, "http"),
-//                        new HttpHost("localhost", 9201, "http")));
-//        final CreateIndexResponse createIndexResponse = client.indices().create(request);
-//        client.close();
+//        LOG.warn(createIndexRequest.validate().getMessage());
+
+//        GetIndexRequest getIndexRequest = new GetIndexRequest();
+
+//        --add-modules java.net.http
+        final HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest;
+        HttpHeaders httpHeaders;
+        HttpResponse<Subscription> httpResponse;
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("sitesearch", System.getenv("SERVICE_SECRET"))
+        );
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost("es.loxal.net", 443, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder -> {
+                    httpClientBuilder.disableAuthCaching();
+                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                });
+
+        final RestHighLevelClient client = new RestHighLevelClient(builder);
+
+        try {
+            LOG.info(">>>>>>>>>>>>>>>>>>>>>>><<<<<<");
+            IndexResponse indexResponse = client.index(indexRequest);
+            assertEquals("OK", indexResponse.status().name());
+            assertEquals(200, indexResponse.status().getStatus());
+//            final boolean exists = client.indices().exists(getIndexRequest);
+//            final CreateIndexResponse createIndexResponse = client.indices().create(createIndexRequest);
+//            client.close();
+        } catch (Exception e) {
+            LOG.error("ERROR >>>>>>>>>>>>>>>>>>>>>> " + e.getCause().getMessage());
+        }
+        assertTrue(true);
     }
 }
