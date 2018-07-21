dca843b53dbd37d25d17995f34714efa12c3828c
Alex <alexander.orlov@loxal.net>
2018-07-21 11:56:44 +0200

    add WordPress test instance

--- ops/run-wordpress.sh
+++ ops/run-wordpress.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env sh
 
-#docker rm -f wordpress-mysql
-#docker run --name wordpress-mysql -d \
-#    -e MYSQL_ROOT_PASSWORD=$SERVICE_SECRET \
-#    --network dev \
-#    mysql:5.7
-#
-#docker rm -f wordpress
-#docker run --name wordpress -it \
-#    --link wordpress-mysql:mysql \
-#    -e WORDPRESS_DB_PASSWORD=$SERVICE_SECRET \
-#    -p 8080:80 \
-#    --network dev \
-#    wordpress:4.9.7-apache
-#
+docker rm -f wordpress-mysql
+docker run --name wordpress-mysql -d \
+    -e MYSQL_ROOT_PASSWORD=$SERVICE_SECRET \
+    --network dev \
+    mysql:5.7
 
-docker network create dev
-docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch down
-docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch up --force-recreate
-docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch ps
+docker rm -f wordpress
+docker run --name wordpress -it \
+    --link wordpress-mysql:mysql \
+    -e WORDPRESS_DB_PASSWORD=$SERVICE_SECRET \
+    -p 7080:80 \
+    --network dev \
+    wordpress:4.9.7-apache
+
+#docker network create dev
+#docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch down
+#docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch up --force-recreate
+#docker-compose --file ./ops/docker-compose-wordpress.yaml -p sitesearch ps
