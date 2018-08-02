f7a8432e83888b3a25c8c25462dad81ade45ebff
Alex <alexander.orlov@loxal.net>
2018-08-02 08:13:45 +0200

    simplify Terraform setup

--- ops/terraform/hetzner-provisioning/main.tf
+++ ops/terraform/hetzner-provisioning/main.tf
@@ -1,35 +1,17 @@
-# to set to switch between zones
-//variable "zones" {
-//    type = "map"
-//    default = {
-//        zone0 = "nbg1-dc3"
-//        zone1 = "fsn1-dc8"
-//    }
-//}
-# you should provide your api token with following command on cli
-# TF_VAR_hcloud_token=your_secret_api_token
-# terraform init
-# terraform plan
-# terraform apply
-# terraform destroy
-
 provider "hcloud" {
-  token = "${local.hcloud_token}"
+  token = "${file("~/.ssh/hetzner-api-token-analyze-law.txt")}"
 }
 
-//resource "hcloud_ssh_key" "default" {
-//  name = "Terraform Example"
+//resource "hcloud_ssh_key" "alex" {
+//  name = "alex"
 //  public_key = "${file("~/.ssh/id_rsa.pub")}"
 //}
 
-resource "hcloud_server" "al" {
+resource "hcloud_server" "node" {
   name = "node-${count.index}"
   count = "1"
-  datacenter = "nbg1-dc3"
+  datacenter = "nbg1-dc2"
   image = "ubuntu-18.04"
   server_type = "cx11-ceph"
 }
 
-locals {
-  hcloud_token = "${file("~/hetzner-api-token-analyze-law.txt")}"
-}
