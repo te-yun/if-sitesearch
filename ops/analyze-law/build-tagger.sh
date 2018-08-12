dedc9af51f7c44926bae41b3b59f426aa30d6dcc
Alex <alexander.orlov@loxal.net>
2018-08-13 00:18:57 +0200

    introduce test-weise JDK 11 build

--- ops/analyze-law/build-tagger.sh
+++ ops/analyze-law/build-tagger.sh
@@ -16,3 +16,5 @@ cd ~/my/project/intrafind/if-sv-clausedetection.svn
 
 sudo cp ~/.m2/repository/com/intrafind/if-sv-clausedetection/0.0.0.1-SNAPSHOT/if-sv-clausedetection-0.0.0.1-SNAPSHOT.jar /home/intrafind/services/if-sv-tagging/
 sudo /etc/init.d/if-sv-tagging restart
+
+http://localhost:9603/json/tagger?method=tag&param0=/home/alex/Downloads/20180726184318.docx&param1=[plainTextSwitch,something]
