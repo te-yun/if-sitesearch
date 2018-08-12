2867d6db33350a693d328ae9f5bc2728798f3bf1
Alex <alexander.orlov@loxal.net>
2018-08-13 00:35:43 +0200

    review/rework inititial glcoud dns update script

--- docker-router/change-sis-cdn-dns-records.sh
+++ docker-router/change-sis-cdn-dns-records.sh
@@ -8,40 +8,43 @@
 # gcloud dns managed-zones list --project woven-alpha-150909
 # gcloud dns record-sets list --zone=sis --project woven-alpha-150909
 
+sudo apt install gcloud -y
+# TODO gcloud init is missing
+# TODO gcloud is not part of the regular packages repository, add a corresponding debian repository first
 # TODO: write "simple" bash script
 
-gcloud dns record-sets transaction start -z sis --project woven-alpha-150909
-gcloud dns record-sets transaction remove -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction start --zone=sis --project woven-alpha-150909
+gcloud dns record-sets transaction remove --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=AAAA \
         --ttl=300 "2600:1901:0:c2e3::"
-gcloud dns record-sets transaction remove -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction remove --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=A \
         --ttl=300 "35.227.226.37"
-gcloud dns record-sets transaction add -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction add --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=A \
         --ttl=300 "35.198.71.123"
-gcloud dns record-sets transaction execute -z sis --project woven-alpha-150909
+gcloud dns record-sets transaction execute --zone=sis --project woven-alpha-150909
 
 # if transaction not successfull reset transaction with below code
-# gcloud dns record-sets transaction abort -z sis --project woven-alpha-150909
+# gcloud dns record-sets transaction abort --zone=sis --project woven-alpha-150909
 
 # update SSL certs
 
 # rollback changes on DNS records
-gcloud dns record-sets transaction start -z sis --project woven-alpha-150909
-gcloud dns record-sets transaction add -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction start --zone=sis --project woven-alpha-150909
+gcloud dns record-sets transaction add --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=AAAA \
         --ttl=300 "2600:1901:0:c2e3::"
-gcloud dns record-sets transaction remove -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction remove --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=A \
         --ttl=300 "35.198.71.123"
-gcloud dns record-sets transaction add -z sis --project woven-alpha-150909 \
+gcloud dns record-sets transaction add --zone=sis --project woven-alpha-150909 \
         --name="cdn.sitesearch.cloud." \
         --type=A \
         --ttl=300 "35.227.226.37"
-gcloud dns record-sets transaction execute -z sis --project woven-alpha-150909
+gcloud dns record-sets transaction execute --zone=sis --project woven-alpha-150909
