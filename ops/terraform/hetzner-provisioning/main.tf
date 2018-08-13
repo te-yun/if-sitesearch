ddd23c45763d4fb13a9ad94bdc40c8257e71145e
Alex <alexander.orlov@loxal.net>
2018-08-13 04:53:28 +0200

    draft AL components via TerraForm

--- ops/terraform/hetzner-provisioning/main.tf
+++ ops/terraform/hetzner-provisioning/main.tf
@@ -14,8 +14,6 @@ resource "hcloud_floating_ip" "main" {
 }
 
 resource "hcloud_server" "node" {
-  //  depends_on = [ "hcloud_floating_ip.main" ]
-
   name = "node-${count.index}"
   count = "1"
   datacenter = "nbg1-dc3"
@@ -41,35 +39,57 @@ resource "hcloud_server" "node" {
     destination = "if-sv-base-installer-1.8.2.17-46323.bin"
   }
 
-  provisioner "file" {
+  provisioner "file" "if-converter-installer" {
     source = "~/Desktop/al-tagger/if-sv-converter-installer-3.7.0.3-52525.bin"
     destination = "if-sv-converter-installer-3.7.0.3-52525.bin"
   }
-  //
-  //  provisioner "file" {
-  //    source = "~/Desktop/al-tagger/if-sv-tagging-installer-2.6.0.5-51045.bin"
-  //    destination = "if-sv-tagging-installer-2.6.0.5-51045.bin"
-  //  }
+
+  provisioner "file" "if-tagging-installer" {
+    source = "~/Desktop/al-tagger/if-sv-tagging-installer-2.6.0.5-51045.bin"
+    destination = "if-sv-tagging-installer-2.6.0.5-51045.bin"
+  }
+
+  provisioner "file" "al-clause-detection" {
+    source = "~/.m2/repository/com/intrafind/if-sv-clausedetection/0.0.0.2-SNAPSHOT/if-sv-clausedetection-0.0.0.2-SNAPSHOT.jar"
+    destination = "if-sv-clausedetection-0.0.0.2-SNAPSHOT.jar"
+  }
+
+  provisioner "file" "al-tagger-resources" {
+    //    tar --exclude .git --exclude .idea -zcvf if-sv-tagging-resources.tgz if-sv-tagging-resources
+    source = "~/my/project/intrafind/if-sv-tagging-resources.tgz"
+    destination = "if-sv-tagging-resources.tgz"
+  }
+
+  provisioner "file" "if-converter-config" {
+    source = "/home/intrafind/services/if-sv-converter/config.cfg"
+    destination = "if-converter-config.cfg"
+  }
+
+  provisioner "file" "contract-example" {
+    source = "/home/alex/Downloads/20180726152906.docx"
+    destination = "20180726152906.docx"
+  }
 
   provisioner "remote-exec" {
     inline = [
-      "apt-get update",
-      "sleep 5",
-      "apt-get install python -y",
+      "apt-get update && apt-get install python -y",
       "chmod +x /root/*.bin",
       "./if-meta-jdk-installer-*.bin -s",
       "./if-sv-base-installer-*.bin -l al-tagger.lic -s",
-      //      "./if-sv-converter-installer-*.bin -l al-tagger.lic -s",
+      "./if-sv-converter-installer-*.bin -l al-tagger.lic -s",
+      "tar xfz if-sv-tagging-resources.tgz; mv if-sv-tagging-resources resources",
+      "./if-sv-tagging-installer-*.bin -l al-tagger.lic -r /root/resources -s",
+      "cp if-sv-clausedetection-*-SNAPSHOT.jar /home/intrafind/services/if-sv-tagging/",
+      "/etc/init.d/if-sv-base restart",
+      "mv if-converter-config.cfg /home/intrafind/services/if-sv-converter/config.cfg",
+      "/etc/init.d/if-sv-converter restart",
+      "export INTRAFIND_LICENSE=/root/al-tagger.lic; export INTRAFIND_LICENSE=/root/al-tagger.lic && /etc/init.d/if-sv-tagging restart",
     ]
   }
 
   provisioner "local-exec" {
     command = "echo ${hcloud_server.node.ipv4_address} > applied-node.txt"
   }
-
-  //    provisioner "local-exec" {
-  //      command = "echo ${hcloud_floating_ip.main.ip_address}  >> applied-main.txt"
-  //    }
 }
 
 resource "hcloud_ssh_key" "minion" {
