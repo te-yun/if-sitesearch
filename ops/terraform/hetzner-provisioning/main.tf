15b0d3d2006cd5d3236a9a93387ffc9acbe64129
Alex <alexander.orlov@loxal.net>
2018-08-15 22:37:33 +0200

    finalize CA deployment

--- ops/terraform/hetzner-provisioning/main.tf
+++ ops/terraform/hetzner-provisioning/main.tf
@@ -34,52 +34,67 @@ resource "hcloud_server" "node" {
 
   provisioner "file" "al-license" {
     source = "~/Desktop/al-tagger/intrafind-dev.lic"
+    //    source = "/root/al-tagger/al-tagger/intrafind-dev.lic"
+    //    source = "main.tf"
+    //    destination = "/root/main.tf"
     destination = "/srv/al-contract-analyzer.lic"
+    //
+    //    connection {
+    //      type     = "ssh"
+    //      user     = "root"
+    //      host = "195.201.29.34"
+    ////      password = "${var.root_password}"
+    //      private_key = "${file("~/.ssh/if-minion-id_rsa")}"
+    //    }
   }
 
   provisioner "file" "if-jdk" {
     source = "~/Desktop/al-tagger/if-meta-jdk-installer-1.8.0.161.bin"
-    destination = "if-meta-jdk-installer-1.8.0.161.bin"
+    destination = "/srv/if-meta-jdk-installer-1.8.0.161.bin"
   }
 
   provisioner "file" "if-base-installer" {
     source = "~/Desktop/al-tagger/if-sv-base-installer-1.8.2.17-46323.bin"
-    destination = "if-sv-base-installer-1.8.2.17-46323.bin"
+    destination = "/srv/if-sv-base-installer-1.8.2.17-46323.bin"
   }
 
   provisioner "file" "if-converter-installer" {
     source = "~/Desktop/al-tagger/if-sv-converter-installer-3.7.0.3-52525.bin"
-    destination = "if-sv-converter-installer-3.7.0.3-52525.bin"
+    destination = "/srv/if-sv-converter-installer-3.7.0.3-52525.bin"
   }
 
   provisioner "file" "if-tagging-installer" {
     source = "~/Desktop/al-tagger/if-sv-tagging-installer-2.6.0.5-51045.bin"
-    destination = "if-sv-tagging-installer-2.6.0.5-51045.bin"
+    destination = "/srv/if-sv-tagging-installer-2.6.0.5-51045.bin"
   }
 
   provisioner "file" "al-clause-detection" {
     source = "~/.m2/repository/com/intrafind/if-sv-clausedetection/0.0.0.2-SNAPSHOT/if-sv-clausedetection-0.0.0.2-SNAPSHOT.jar"
-    destination = "if-sv-clausedetection-0.0.0.2-SNAPSHOT.jar"
+    destination = "/srv/if-sv-clausedetection-0.0.0.2-SNAPSHOT.jar"
   }
 
   provisioner "file" "al-tagger-resources" {
     //    tar --exclude .git --exclude .idea -zcvf if-sv-tagging-resources.tgz if-sv-tagging-resources
     source = "~/my/project/intrafind/if-sv-tagging-resources.tgz"
-    destination = "if-sv-tagging-resources.tgz"
+    destination = "/srv/if-sv-tagging-resources.tgz"
   }
 
   provisioner "file" "if-converter-config" {
     source = "/home/intrafind/services/if-sv-converter/config.cfg"
-    destination = "if-converter-config.cfg"
+    destination = "/srv/if-converter-config.cfg"
   }
 
   provisioner "file" "contract-example" {
     source = "~/Downloads/20180726152906.docx"
-    destination = "20180726152906.docx"
+    destination = "/srv/20180726152906.docx"
   }
 
   provisioner "remote-exec" {
-    script = "setup.sh"
+    //    script = "setup.sh"
+    scripts = [
+      //      "provide-artifacts.sh",
+      "setup.sh"
+    ]
   }
 
   provisioner "local-exec" "server" {
--- ops/terraform/hetzner-provisioning/setup.sh
+++ ops/terraform/hetzner-provisioning/setup.sh
@@ -2,33 +2,25 @@
 
 export INTRAFIND_LICENSE=/srv/al-contract-analyzer.lic
 echo 'export INTRAFIND_LICENSE=/srv/al-contract-analyzer.lic' >> .bashrc
+cd /srv
 apt-get update && apt-get install python -y
-chmod +x /root/*.bin
+chmod +x /srv/*.bin
 ./if-meta-jdk-installer-*.bin -s
 ./if-sv-base-installer-*.bin -l /dev/null -s
 ./if-sv-converter-installer-*.bin -l /dev/null -s
-#tar xfz if-sv-tagging-resources.tgz; mv if-sv-tagging-resources resources
 tar xfz if-sv-tagging-resources.tgz
-./if-sv-tagging-installer-*.bin -l $INTRAFIND_LICENSE -r /root/if-sv-tagging-resources -s
-cp -r /root/resources /home/intrafind/services/if-sv-tagging/
+./if-sv-tagging-installer-*.bin -l $INTRAFIND_LICENSE -r /srv/if-sv-tagging-resources -s
+cp -r /srv/resources /home/intrafind/services/if-sv-tagging/
 cp if-sv-clausedetection-*-SNAPSHOT.jar /home/intrafind/services/if-sv-tagging/
 cp $INTRAFIND_LICENSE /home/intrafind/services/if-sv-tagging/resources/intrafind.lic
 mv if-converter-config.cfg /home/intrafind/services/if-sv-converter/config.cfg
 echo 'common.config-store: com.intrafind.common.config.configstore.ConfigStores.locationExt("configstore/config-ext.cfg");' \
     >> /home/intrafind/services/if-sv-tagging/resources/config.cfg
-/etc/init.d/if-sv-tagging restart; sleep 15;
-/etc/init.d/if-sv-base restart; sleep 15
+/etc/init.d/if-sv-base restart
 /etc/init.d/if-sv-converter restart
-/etc/init.d/if-sv-tagging restart; sleep 15;
-#/etc/init.d/if-sv-base restart; sleep 15;
-#/etc/init.d/if-sv-converter restart; sleep 15
-#/etc/init.d/if-sv-tagging restart; sleep 15;
-#/etc/init.d/if-sv-base restart; sleep 15;
-#/etc/init.d/if-sv-converter restart; sleep 15;
+/etc/init.d/if-sv-tagging restart
 cat /home/intrafind/services/if-sv-tagging/logs/2018-*.log
-#curl 'http://localhost:9603/json/tagger?method=tag&param0=/root/20180726152906.docx'
-#ls /home/intrafind/
+wget 'http://localhost:9603/json/tagger?method=tag&param0=/srv/20180726152906.docx&param1=[plainTextSwitch,something]'
+ls /home/intrafind/
 
-# sleep 15; export INTRAFIND_LICENSE=/root/al-contract-analyzer.lic && /etc/init.d/if-sv-tagging restart
-# export INTRAFIND_LICENSE=/root/al-contract-analyzer.lic; export INTRAFIND_LICENSE=/root/al-contract-analyzer.lic && /etc/init.d/if-sv-tagging restart
 # TODO install ZIP IF Installer
