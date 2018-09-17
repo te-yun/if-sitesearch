516f88008e4bd26697f77bc1708dd4471558177c
bachka <ichinnorovb@gmail.com>
2018-09-17 08:02:32 -0700

    clean code for searchbar update script

--- ci/update-searchbar.sh
+++ ci/update-searchbar.sh
@@ -7,15 +7,8 @@
 
 searchbarVersion=`date -u +"%Y-%m-%d"`
 
-#function init_and_set_CDN_metadata() {
-#    gsutil cors set cdn-cors-configuration.json gs://site-search-europe
-#    gsutil setmeta  -h "content-encoding"  gs://site-search-europe/searchbar/$searchbarVersion/app/css/app.css
-#    gsutil -m setmeta -r -h "Content-Encoding: gzip"  gs://site-search-europe/searchbar/$searchbarVersion
-#    gsutil -m acl -r ch -u AllUsers:READER gs://site-search-europe/searchbar/$searchbarVersion
-#    gsutil -m iam -r ch allUsers:objectViewer gs://site-search-europe/$searchbarVersion
-#}
-
-function get_latest_searchbar_release(){
+function get_latest_searchbar_release() {
+    rm -rf $searchbarVersion
     mkdir -p $searchbarVersion/app
     wget -O latestSearchbarRelease.xml http://ml-if-nexus:8081/repository/releases/com/intrafind/if-app-searchbar/maven-metadata.xml
     sed -i '/<versions>/,/<\/versions>/d' latestSearchbarRelease.xml
@@ -24,33 +17,33 @@ function get_latest_searchbar_release(){
     unzip -qq if-app-searchbar-$latestRelease.war -d $searchbarVersion/app -x "META-INF/*" "WEB-INF/*"
     rm if-app-searchbar-$latestRelease.war
 }
-# get_latest_searchbar_release
+get_latest_searchbar_release
 
-function get_old_release_deployment_fragments(){
-    rm -rf ./service/src/main/resources/static/searchbar/$searchbarVersion
-    mv $searchbarVersion/ ./service/src/main/resources/static/searchbar/
-    mv ./service/src/main/resources/static/searchbar/latestSearchbarRelease.xml ./service/src/main/resources/static/searchbar/oldSearchbarRelease.xml
-    mv latestSearchbarRelease.xml ./service/src/main/resources/static/searchbar/
-    oldReleaseDate=$(cat ./service/src/main/resources/static/searchbar/oldSearchbarRelease.xml | sed -ne '/<lastUpdated>/s#\s*<[^>]*>\s*##gp')
+function get_old_release_deployment_fragments() {
+    rm -rf service/src/main/resources/static/searchbar/$searchbarVersion/
+    mv $searchbarVersion/ service/src/main/resources/static/searchbar/
+    mv service/src/main/resources/static/searchbar/latestSearchbarRelease.xml service/src/main/resources/static/searchbar/oldSearchbarRelease.xml
+    mv latestSearchbarRelease.xml service/src/main/resources/static/searchbar/
+    oldReleaseDate=$(cat service/src/main/resources/static/searchbar/oldSearchbarRelease.xml | sed -ne '/<lastUpdated>/s#\s*<[^>]*>\s*##gp')
     oldReleaseDate=${oldReleaseDate:0:8}
     oldReleaseDate=`date -d $oldReleaseDate +'%Y-%m-%d'`
-    cp -r ./service/src/main/resources/static/searchbar/$oldReleaseDate/config/ ./service/src/main/resources/static/searchbar/$searchbarVersion/
-    cp -r ./service/src/main/resources/static/searchbar/$oldReleaseDate/gadget/ ./service/src/main/resources/static/searchbar/$searchbarVersion/
+    cp -r service/src/main/resources/static/searchbar/"$oldReleaseDate"/config/ service/src/main/resources/static/searchbar/$searchbarVersion/
+    cp -r service/src/main/resources/static/searchbar/"$oldReleaseDate"/gadget/ service/src/main/resources/static/searchbar/"$searchbarVersion"/
 }
-# get_old_release_deployment_fragments
+get_old_release_deployment_fragments
 
-function config_new_release_data(){
-    sed -i -e 's/$oldReleaseDate/$searchbarVersion/g' ./service/src/main/resources/static/searchbar/$searchbarVersion/config/sitesearch.json
-    sed -i -e 's/$oldReleaseDate/$searchbarVersion/g' ./service/src/main/resources/static/searchbar/$searchbarVersion/gadget/main.xml
-    sed -i -e 's/$oldReleaseDate/$searchbarVersion/g' ./service/src/main/resources/static/searchbar/integration.html
+function config_new_release_data() {
+    sed -i -e "s/$oldReleaseDate/$searchbarVersion/g" service/src/main/resources/static/searchbar/"$searchbarVersion"/config/sitesearch.json
+    sed -i -e "s/$oldReleaseDate/$searchbarVersion/g" service/src/main/resources/static/searchbar/"$searchbarVersion"/gadget/main.xml
+    sed -i -e "s/$oldReleaseDate/$searchbarVersion/g" service/src/main/resources/static/searchbar/integration.html
 }
-# config_new_release_data
+config_new_release_data
 
-function deploy_searchbar_on_cdn(){
+function deploy_searchbar_on_cdn() {
     gsutil -m rm -r gs://site-search-europe/searchbar/$searchbarVersion
     gsutil -m cp -r ./service/src/main/resources/static/searchbar/$searchbarVersion gs://site-search-europe/searchbar/
     gsutil cp -z css -a public-read ./service/src/main/resources/static/searchbar/$searchbarVersion/app/css/app.css gs://site-search-europe/searchbar/$searchbarVersion/app/css/
     gsutil cp -z js -a public-read ./service/src/main/resources/static/searchbar/$searchbarVersion/app/js/app.js gs://site-search-europe/searchbar/$searchbarVersion/app/js/
     gsutil cp -z json -a public-read ./service/src/main/resources/static/searchbar/$searchbarVersion/config/sitesearch.json gs://site-search-europe/searchbar/$searchbarVersion/config/
 }
-# deploy_searchbar_on_cdn
+deploy_searchbar_on_cdn
