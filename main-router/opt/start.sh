d49eab9e69adf63937ad161bfd98309453b257ed
Alex <alexander.orlov@loxal.net>
2018-11-02 10:07:14 +0100

    introduce new router logic

--- main-router/opt/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-nginx -g 'daemon off;' &
-echo -n $LIVE > /var/live
-consul-template -consul-addr=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload"
