74c4df591c3ccf33f67b4a00794b739d424eed7b
Alex <alexander.orlov@loxal.net>
2018-11-03 20:28:37 +0100

    cleanup main router

--- release-main-router.sh
+++ release-main-router.sh
@@ -10,19 +10,3 @@ sudo cp -r /etc/letsencrypt .
 sudo docker build --no-cache --pull --tag docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag} .
 sudo rm -rf letsencrypt
 docker push docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag}
-
-
-#docker rm -f $docker_redirect_image
-#docker run -d --name $docker_redirect_image \
-#    -p 80:80 \
-#    -p 443:443 \
-#    --network $docker_network \
-#    docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag}
-#
-#docker rm -f docker-registry
-#docker run -d --name docker-registry \
-#    -v /srv/registry1:/var/lib/registry \
-#    --restart unless-stopped \
-#    --network $docker_network \
-#    registry:2
-
---
+++ run-main-router.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+docker_network=main
+docker_redirect_image=main-router
+docker_tag=latest
+
+docker rm -f $docker_redirect_image
+#docker rmi -f f4e9c1e52aa6
+docker run -d --name $docker_redirect_image \
+    -p 80:80 \
+    -p 443:443 \
+    --network $docker_network \
+    docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag}
+
+docker rm -f docker-registry
+docker run -d --name docker-registry \
+    -v /srv/registry1:/var/lib/registry \
+    --restart unless-stopped \
+    --network $docker_network \
+    registry:2
+
