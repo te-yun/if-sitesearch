5d76c118cd8bfee94a332e58c23a14b173405510
Alex <alexander.orlov@loxal.net>
2018-12-05 21:30:33 +0100

    cleanup

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -46,6 +46,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2018 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 //buildscript {
 //        ext {
@@ -116,7 +132,7 @@ jmh {
     include = listOf("LoadTest")
     warmupIterations = 1
     warmupForks = 0
-    fork = 0
+    fork = 1
     iterations = 5
     resultFormat = "JSON"
     threads = 10
