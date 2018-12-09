0a57da654d4032789b1a16a9a157f9553352776e
Alex <alexander.orlov@loxal.net>
2018-12-09 17:19:50 +0100

    adjust ELK

--- ops/start-or-update-docker-registry.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-docker-compose --file opt/docker-compose-infrastructure.yaml -p tmp down
-docker-compose --file opt/docker-compose-infrastructure.yaml -p tmp up -d --force-recreate
-docker-compose --file opt/docker-compose-infrastructure.yaml -p tmp ps
